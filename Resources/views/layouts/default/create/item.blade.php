8c62a8ed1b3197ae72439f2c5b5eeacff7145417
marco76tv <marco.sottana@gmail.com>
2019-08-12 21:29:24 +0200

    .

--- Resources/views/layouts/default/create/item.blade.php
+++ Resources/views/layouts/default/create/item.blade.php
@@ -1,3 +1,4 @@
+{{--
 {!! Form::bsOpen($row,'store') !!}
 <div class="row">
 @foreach($_panel->fields() as $field)
@@ -11,6 +12,32 @@
 	<div class="col-sm-{{ isset($field->col_bs_size)?$field->col_bs_size:12 }}">
 	{!! Form::$input($input_name,$input_value) !!}
 	</div>
+@endforeach
+</div>
+{{Form::bs3Submit('')}}
+{!! Form::close() !!}
+--}}
+@php
+	if(!is_object($row)) return '';
+	$fields=$_panel->createFields();
+@endphp
+
+{!! Form::bsOpen($row,'store') !!}
+<div class="row">
+@foreach($fields as $field)
+{{--  
+@php
+	$input='bs'.studly_case($field->type);
+	$input_name=collect(explode('.',$field->name))->map(function ($v, $k){
+		return $k==0?$v:'['.$v.']';
+	})->implode('');
+	$input_value=(isset($field->value)?$field->value:null);
+@endphp
+	<div class="col-sm-{{ isset($field->col_bs_size)?$field->col_bs_size:12 }}">
+	{!! Form::$input($input_name,$input_value) !!}
+	</div>
+--}}
+	{!! Theme::inputHtml(['row'=>$row,'field'=>$field]) !!}
 @endforeach
 </div>
 {{Form::bs3Submit('')}}
--- Resources/views/layouts/default/edit/item.blade.php
+++ Resources/views/layouts/default/edit/item.blade.php
@@ -1,3 +1,4 @@
+{{--
 {!! Form::bsOpen($row,'update') !!}
 <div class="row">
 @foreach($_panel->fields() as $field)
@@ -14,4 +15,18 @@
 @endforeach
 </div>
 {{Form::bs3Submit('Modifica')}}
+{!! Form::close() !!}
+--}}
+@php
+	if(!is_object($row)) return '';
+	$fields=$_panel->editFields();
+@endphp
+
+{!! Form::bsOpen($row,'update') !!}
+<div class="row">
+@foreach($fields as $field)
+	{!! Theme::inputHtml(['row'=>$row,'field'=>$field]) !!}
+@endforeach
+</div>
+{{Form::bs3Submit('')}}
 {!! Form::close() !!}
