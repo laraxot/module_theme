a79ca6ddfc81df22c4dc198ec39c1e363deefa66
marco76tv <marco.sottana@gmail.com>
2019-08-17 01:36:17 +0200

    .

--- Resources/views/layouts/default/index_edit/body_content.blade.php
+++ Resources/views/layouts/default/index_edit/body_content.blade.php
@@ -1,3 +1,9 @@
+<div class="row">
+    <div class="col">
+        <p><span class="primary-color"><strong>{{ number_format($rows->total(),0,',',' ') }}</strong></span> {{ Str::plural($row->post_type) }}</p>
+    </div>
+	@include('pub_theme::layouts.partials.forms.search')
+</div>
 {!! Form::bsBtnCreate(['row'=>$row]) !!}
 @foreach($rows as $key=>$row)
 	@include($_layout->item_view,['key'=>$key,'row'=>$row])
