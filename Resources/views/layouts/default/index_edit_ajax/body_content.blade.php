706b76a58adbcf7e13404e4b63dcc363828ceb63
marco76tv <marco.sottana@gmail.com>
2019-08-30 23:40:38 +0200

    .

--- Resources/views/layouts/default/index_edit_ajax/body_content.blade.php
+++ Resources/views/layouts/default/index_edit_ajax/body_content.blade.php
@@ -1,14 +1,5 @@
-{{--
-@foreach($rows  as $key=>$row)
-	@include($_layout->item_view,['key'=>$key,'row'=>$row])
-@endforeach
-{{ $rows->links('pub_theme::layouts.partials.pagination') }} 
---}}
 @php
-$types=camel_case(str_plural($params['container1']));
-$last_item=$params['item0'];
- Theme::add('/theme/pub/dist/js/app.js',1);
- //ddd($_panel);
+	Theme::add('/theme/pub/dist/js/app.js',1);
 @endphp
 
 {{--  
