0bdd1e9f13c53238039a0561583d1afdc42f060a
marco76tv <marco.sottana@gmail.com>
2019-09-17 22:43:58 +0200

    .

--- Resources/views/metatags.blade.php
+++ Resources/views/metatags.blade.php
@@ -2,24 +2,26 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1,minimum-scale=1">
 <meta name="apple-mobile-web-app-capable" content="yes">
-<link rel="icon" type="image/x-icon" href="{{ asset('fav.png') }}" />
+{{--
+<link rel="icon" type="image/x-icon" href="{{ Theme::asset('pub_theme::icons/fav.png') }}" />
+--}}
 <meta name="author" content="{{ Theme::metatag('author') }}">
 <link rel="author" href="https://plus.google.com/b/104035968244380319088/?hl=it" />
-<link rel="icon" href="{{ asset('favicon.ico') }}" />
-<link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
-<link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png">
-<link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png">
-<link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png">
-<link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png">
-<link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png">
-<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png">
-<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
-<link rel="icon" type="image/png" href="/favicon-194x194.png" sizes="194x194">
-<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
-<link rel="icon" type="image/png" href="/android-chrome-192x192.png" sizes="192x192">
-<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+<link rel="icon" href="{{ Theme::asset('pub_theme::icons/favicon.ico') }}" />
+<link rel="apple-touch-icon" sizes="57x57" href="{{ Theme::asset('pub_theme::icons/apple-touch-icon-57x57.png') }}">
+<link rel="apple-touch-icon" sizes="60x60" href="{{ Theme::asset('pub_theme::icons/apple-touch-icon-60x60.png') }}">
+<link rel="apple-touch-icon" sizes="72x72" href="{{ Theme::asset('pub_theme::icons/apple-touch-icon-72x72.png') }}">
+<link rel="apple-touch-icon" sizes="76x76" href="{{ Theme::asset('pub_theme::icons/apple-touch-icon-76x76.png') }}">
+<link rel="apple-touch-icon" sizes="114x114" href="{{ Theme::asset('pub_theme::icons/apple-touch-icon-114x114.png') }}">
+<link rel="apple-touch-icon" sizes="120x120" href="{{ Theme::asset('pub_theme::icons/apple-touch-icon-120x120.png') }}">
+<link rel="apple-touch-icon" sizes="144x144" href="{{ Theme::asset('pub_theme::icons/apple-touch-icon-144x144.png') }}">
+<link rel="apple-touch-icon" sizes="152x152" href="{{ Theme::asset('pub_theme::icons/apple-touch-icon-152x152.png') }}">
+<link rel="apple-touch-icon" sizes="180x180" href="{{ Theme::asset('pub_theme::icons/apple-touch-icon-180x180.png') }}">
+<link rel="icon" type="image/png" href="{{ Theme::asset('pub_theme::icons/favicon-32x32.png') }}" sizes="32x32">
+<link rel="icon" type="image/png" href="{{ Theme::asset('pub_theme::icons/favicon-194x194.png') }}" sizes="194x194">
+<link rel="icon" type="image/png" href="{{ Theme::asset('pub_theme::icons/favicon-96x96.png') }}" sizes="96x96">
+<link rel="icon" type="image/png" href="{{ Theme::asset('pub_theme::icons/android-chrome-192x192.png') }}" sizes="192x192">
+<link rel="icon" type="image/png" href="{{ Theme::asset('pub_theme::icons/favicon-16x16.png') }}" sizes="16x16">
 {{--    DA RIABILITARE QUANDO AVRA' SENSO 
 <link rel="manifest" href="{{ asset('/manifest.json') }}">
 --}}
