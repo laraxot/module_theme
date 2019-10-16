fe1dd93c10b336493e0de29a0516899fcb314878
marco76tv <marco.sottana@gmail.com>
2019-10-16 10:58:51 +0200

    .

--- Resources/views/layouts/default/admin/index/body_content.blade.php
+++ Resources/views/layouts/default/admin/index/body_content.blade.php
@@ -1,35 +1,29 @@
-{!! Form::bsBtnCreateAttach(['row'=>$row]) !!} 
+<div class="row text-dark" style="background-color: #e9ecef; padding:5px; ">
+	{!! Form::bsBtnCreateAttach(['row'=>$row]) !!} 
+	@can('create',$row)
+	<a data-href="{{ Panel::get($row)->createUrl() }}" class="btn btn-primary" data-toggle="modal" data-target="#myModalAjax" data-title="plus">
+		<i class="fa fa-plus"></i> 
+	</a>
+	@endcan
+	@foreach($_panel->containerActions() as $act)
+		{!! $act->btn() !!}
+	@endforeach
 
-
-<a data-href="{{ Panel::get($row)->createUrl() }}" class="btn btn-primary" data-toggle="modal" data-target="#myModalAjax" data-title="plus">
-	<i class="fa fa-plus"></i> 
-</a>
-
-@foreach($_panel->containerActions() as $act)
-	{!! $act->btn() !!}
-@endforeach
-{{--
-@include('adm_theme::layouts.partials.forms.search') 
---}}
-<div class="row">
     <div class="col">
-        <p><span class="primary-color"><strong>{{ number_format($rows->total(),0,',',' ') }}</strong></span> 
-        	{{ Str::plural($row->post_type ?? class_basename($row),$rows->total()) }}</p>
+        <p>
+        	<span class="primary-color"><strong>{{ number_format($rows->total(),0,',',' ') }}</strong></span> 
+        	{{ Str::plural($row->post_type ?? class_basename($row),$rows->total()) }}
+        </p>
     </div>
-    {{--  
-	@include('adm_theme::layouts.partials.forms.search')
-	--}}
 	@include('formx::includes.components.form_complete.search')
 	@include('formx::includes.components.form_complete.order_by')
-	
 </div>
-
+<br/>
 <div class="row">
     <div class="col" style="overflow:auto">
-@foreach($rows as $k=>$v)
-	@include($_layout->item_view,['key'=>$k,'row'=>$v])
-@endforeach
-{{ $rows->appends(request()->query())->links() }}
-</div>
-  
+		@foreach($rows as $k=>$v)
+			@include($_layout->item_view,['key'=>$k,'row'=>$v])
+		@endforeach
+		{{ $rows->appends(request()->query())->links() }}
+	</div>
 </div>
