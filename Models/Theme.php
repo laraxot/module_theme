392cfe4e377cc8ab2561adbbba99687ebbea137a
marco76tv <marco.sottana@gmail.com>
2020-02-17 18:51:24 +0100

    .

--- Models/Theme.php
+++ Models/Theme.php
@@ -4,7 +4,8 @@ namespace Modules\Theme\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Theme extends Model {
+class Theme extends Model
+ {
     protected $fillable=['id','title','version','txt','link','status','created_at','created_by','updated_at','updated_by'];
 
 
