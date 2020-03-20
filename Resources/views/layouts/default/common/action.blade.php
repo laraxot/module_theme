5f43f956e25e83fd21cf689754124636c4369ecb
marco76tv <marco.sottana@gmail.com>
2020-03-20 23:18:06 +0100

    .

--- Resources/views/layouts/default/common/action.blade.php
+++ Resources/views/layouts/default/common/action.blade.php
@@ -6,22 +6,17 @@
 @php
 	$panel=Panel::get($row);
 @endphp
-
 {!! Form::bsBtnGear(['row'=>$row]) !!}
 
 <div class="page-wrapper">
     {!! Theme::include('inner_page',[],get_defined_vars() ) !!}
-    {{--
-    {!! Theme::include('breadcrumb',[],get_defined_vars() ) !!}
-    {!! Theme::include('tabs',['tabs'=>$panel->getTabs()],get_defined_vars() ) !!}
-    --}}
     @include('pub_theme::layouts.partials.breadcrumb')
     @include('pub_theme::layouts.partials.tabs',['tabs'=>$panel->getTabs()])
 	<section class="create-page inner-page">
-		<div class="container">
-		{!! Theme::include('topbar',[],get_defined_vars() ) !!}
+        <div class="container">
+            {!! Theme::include('topbar',[],get_defined_vars() ) !!}
 			<div class="row">
-				@include('theme::layouts.default.common.'.$_layout->view_body)
+                @include('theme::layouts.default.common.'.$_layout->view_body)
 			</div>
 		</div>
 	</section>
--- Resources/views/layouts/default/index/body_content.blade.php
+++ Resources/views/layouts/default/index/body_content.blade.php
@@ -1,15 +1,3 @@
-{{--
-@php
-	//Str::plural($row->post_type)
-@endphp
-<div class="row">
-    <div class="col">
-        <p><span class="primary-color"><strong>{{ number_format($rows->total(),0,',',' ') }}</strong></span> {{ Str::plural($_panel->postType()) }}</p>
-    </div>
-	@include('pub_theme::layouts.partials.forms.search')
-</div>
---}}
-
 @foreach($rows  as $key=>$row)
 	@include($_layout->item_view,['key'=>$key,'row'=>$row])
 @endforeach
