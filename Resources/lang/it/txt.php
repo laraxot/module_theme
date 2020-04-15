92ddc013e087fbc6e36cc95b2df4f7e5e7f1bd28
Malebestia <malebestia@yahoo.it>
2020-04-15 19:57:55 +0200

    .

--- Resources/lang/it/txt.php
+++ Resources/lang/it/txt.php
@@ -9,6 +9,27 @@ return [
         4=>'Giovedì',
         5=>'Venerdì',
         6=>'Sabato',
-    ]
+    ],
 
-    ];
+    'sort'=>[
+        'result' => 'risultati trovati',
+        'sort_by' => 'Ordina per',
+        "distance|asc" => 'Distanza',
+        "ratings_avg|desc" => 'Valutazione',
+        "ratings_count|desc" => 'Numero valutazioni',
+        "updated_at|desc" => 'Creazione',
+    ],
+
+    'search'=>[
+        'keyword' => 'Parola chiave',
+        'city' => 'Città',
+        'category' => 'Categoria',
+        'cuisine' => 'Piatti',
+        'more_filter' => 'Aggiungi filtri',
+        'less_filter' => 'Togli filtri',
+        'filter' => 'Filtra',
+    ],
+
+    'rating' => 'Valutazione',
+
+];
