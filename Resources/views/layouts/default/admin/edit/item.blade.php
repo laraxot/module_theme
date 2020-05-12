abc65f56e6c7999c956f91bfea5ce7046a017979
marco76tv <marco.sottana@gmail.com>
2020-05-12 19:07:20 +0200

    .

--- Resources/views/layouts/default/admin/edit/item.blade.php
+++ Resources/views/layouts/default/admin/edit/item.blade.php
@@ -1,10 +1,8 @@
 @php
 	if(!is_object($row)) return '';
     $fields=$_panel->editFields();
-
-    //ddd($row->pivot->jobRoles);
-    @endphp
-{!! Form::bsOpen($row,'update') !!}
+@endphp
+{!! Form::bsOpenPanel($_panel,'update') !!}
 <div class="row">
 @foreach($fields as $field)
     {!! Theme::inputHtml(['row'=>$row,'field'=>$field]) !!}
