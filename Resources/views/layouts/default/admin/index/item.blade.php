c9a4f4763984c5d6274ab776459266fc28dfdc08
marco76tv <marco.sottana@gmail.com>
2020-05-26 14:34:12 +0200

    .

--- Resources/views/layouts/default/admin/index/item.blade.php
+++ Resources/views/layouts/default/admin/index/item.blade.php
@@ -27,7 +27,11 @@
 
                 @if($loop->first)
                 @foreach($_panel->itemActions() as $act)
-                {!! $act->btn(['row'=>$row,'panel'=>Panel::get($row)]) !!}
+                {{--
+                    {!! $act->btn(['row'=>$row,'panel'=>Panel::get($row)]) !!}
+                --}}
+
+                {!! $act->btnHtml() !!}
                 @endforeach
                 @endif
 			</td>
