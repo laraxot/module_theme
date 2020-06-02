6d77eb990f6b61770aa4921add68b8b95eb373cc
marco76tv <marco.sottana@gmail.com>
2020-06-02 16:02:06 +0200

    .

---
+++ Resources/smarty/plugins/function.get_ciao.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Smarty plugin
+ * -------------------------------------------------------------
+ * File:     function.get_ciao.php
+ * Type:     function
+ * Name:     get_ciao
+ * Purpose:  get ciao
+ * -------------------------------------------------------------
+ * https://www.smarty.net/best_practices
+ */
+function smarty_function_get_ciao($params, $smarty) {
+    return 'ciao';
+}
