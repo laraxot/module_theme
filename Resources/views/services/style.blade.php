320b4af2b5c6f8302f9b9db2da274796f3bd74ec
marco76tv <marco.sottana@gmail.com>
2020-08-12 19:44:01 +0200

    .

--- Resources/views/services/style.blade.php
+++ Resources/views/services/style.blade.php
@@ -1,3 +1,3 @@
 @foreach($styles as $style)
-<link href="{{asset($style)}}" rel="stylesheet" type="text/css" />
+<link href="{{asset($style)}}?{{ rand(1,10) }}" rel="stylesheet" type="text/css" />
 @endforeach
