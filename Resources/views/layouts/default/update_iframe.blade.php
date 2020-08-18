0e4029fd072a5b6bcc5a86099be1bca3ca33a7f7
Malebestia <malebestia@yahoo.it>
2020-08-18 18:02:18 +0200

    .

--- Resources/views/layouts/default/update_iframe.blade.php
+++ Resources/views/layouts/default/update_iframe.blade.php
@@ -1,14 +1,1 @@
-@extends('pub_theme::layouts.app_iframe')
-@section('content')
-@include('theme::includes.flash')
-@php
-$params['format']='iframe';
-@endphp
-<a href="{{ route('container0.container1.index_edit',$params) }}" class="btn btn-secondary">Torna alla Lista</a>
-
-<a href="{{ route('container0.container1.edit',$params) }}" class="btn btn-secondary">Torna a modificare</a>
-
-<a href="{{ route('container0.container1.create',$params) }}" class="btn btn-secondary">Crea Nuovo</a>
-
- 
-@endsection
+@include('theme::layouts.default.common.action_iframe')
