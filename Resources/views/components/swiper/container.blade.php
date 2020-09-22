7300499ebda6b1a1baefc4d033cf670dcea3a14e
marco76tv <marco.sottana@gmail.com>
2020-09-22 12:30:29 +0200

    theme components

---
+++ Resources/views/components/swiper/container.blade.php
@@ -0,0 +1,17 @@
+{{--  
+https://github.com/nolimits4web/Swiper/blob/master/demos/010-default.html
+--}}
+ <div class="swiper-container">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide">Slide 1</div>
+      <div class="swiper-slide">Slide 2</div>
+      <div class="swiper-slide">Slide 3</div>
+      <div class="swiper-slide">Slide 4</div>
+      <div class="swiper-slide">Slide 5</div>
+      <div class="swiper-slide">Slide 6</div>
+      <div class="swiper-slide">Slide 7</div>
+      <div class="swiper-slide">Slide 8</div>
+      <div class="swiper-slide">Slide 9</div>
+      <div class="swiper-slide">Slide 10</div>
+    </div>
+  </div>
