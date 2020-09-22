820c043a7c29b8737df5038d2640856a7f748eda
marco76tv <marco.sottana@gmail.com>
2020-09-22 12:01:38 +0200

    bs4 components

---
+++ Resources/views/components/badge.blade.php
@@ -0,0 +1,8 @@
+{{-- 
+https://getbootstrap.com/docs/4.5/components/badge/
+ --}}
+
+ <button type="button" class="btn btn-primary">
+  Profile <span class="badge badge-light">9</span>
+  <span class="sr-only">unread messages</span>
+</button>
