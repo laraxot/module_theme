dcaaa162483fd82e2da8cc8ca509128a1f8a100f
Malebestia <malebestia@yahoo.it>
2020-10-02 13:05:49 +0200

    .

--- Resources/views/components/modal/simple.blade.php
+++ Resources/views/components/modal/simple.blade.php
@@ -4,7 +4,7 @@
        <div class="modal-content">
            <div class="modal-header">
                <h5 class="modal-title" id="{{ $guid }}Label">{{ $title }}</h5>
-               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+               <button type="button" class="close" data-dismiss="modal" aria-label="Close" wire:click.prevent="cancel()">
                    <span aria-hidden="true">×</span>
                 </button>
             </div>
