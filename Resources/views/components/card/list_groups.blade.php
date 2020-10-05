9101ceed61c20cac894ae8134a38b185910d078b
marco76tv <marco.sottana@gmail.com>
2020-10-05 21:19:14 +0200

    .

--- Resources/views/components/card/list_groups.blade.php
+++ Resources/views/components/card/list_groups.blade.php
@@ -1,13 +1,18 @@
-<div class="card" style="width: 18rem;">
-<div class="card-header">
-    Featured
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">Cras justo odio</li>
-    <li class="list-group-item">Dapibus ac facilisis in</li>
-    <li class="list-group-item">Vestibulum at eros</li>
-  </ul>
-  <div class="card-footer">
-    Card footer
-  </div>
+<div class="card" {!! null !=='style' ? 'style="' . $style . '"' : '' !!}>
+    <div class="card-header">
+        {{ $title }}
+    </div>
+    <div class="card-body">
+        {{ $content }}
+    </div>
+    <ul class="list-group list-group-flush">
+        @foreach ($items as $item)
+            <li class="list-group-item">{!! $item !!}</li>
+        @endforeach
+    </ul>
+    {{--
+    <div class="card-footer">
+        Card footer
+    </div>
+    --}}
 </div>
