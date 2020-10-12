a1c899776ab9f2af097864ad67a24e460b4f0ed3
Malebestia <malebestia@yahoo.it>
2020-10-12 20:52:39 +0200

    .

--- Http/Livewire/VerMenu.php
+++ Http/Livewire/VerMenu.php
@@ -3,19 +3,19 @@
 namespace Modules\Theme\Http\Livewire;
 
 use Livewire\Component;
-use Livewire\WithPagination;
+use Modules\Xot\Services\TenantService;
 
 //use Modules\Cart\Models\BookingItem;
 
 class VerMenu extends Component {
-    use WithPagination;
-
     public function render() {
         $view = 'theme::livewire.ver_menu';
         $view_params = [
             'view' => $view,
-            'areas' => TenantService::model('area')->get(),
+            //'areas' => TenantService::model('area')->get(),
+            'areas' => TenantService::model('area')->pluck('nome')->all(),
         ];
+        //dddx(TenantService::model('area')->pluck('nome')->all());
 
         return view($view, $view_params);
     }
