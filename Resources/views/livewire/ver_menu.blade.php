887e3fc2d23c77c80d9850bedc0af7d32b0fd482
Malebestia <malebestia@yahoo.it>
2020-10-13 12:52:31 +0200

    .

--- Resources/views/livewire/ver_menu.blade.php
+++ Resources/views/livewire/ver_menu.blade.php
@@ -1,8 +1,6 @@
 <div>
 
-<div class="aside-menu-wrapper flex-column-fluid" id="kt_aside_menu_wrapper">
-	<div id="kt_aside_menu" class="aside-menu my-4 scroll ps ps--active-y" data-menu-vertical="1" data-menu-scroll="1" data-menu-dropdown-timeout="500" style="height: 698px; overflow: hidden;">
-		<ul class="menu-nav ">
+
 			<li class="menu-item  menu-item-active" aria-haspopup="true">
 				<a href="http://localhost/food_tribu_project/mecronic_one_laravel/public" class="menu-link ">
 					<span class="svg-icon menu-icon">
@@ -924,14 +922,6 @@
 				</div>
                 --}}
             </li>
-		</ul>
-		<div class="ps__rail-x" style="left: 0px; bottom: 0px;">
-			<div class="ps__thumb-x" tabindex="0" style="left: 0px; width: 0px;"></div>
-		</div>
-		<div class="ps__rail-y" style="top: 0px; height: 698px; right: 4px;">
-			<div class="ps__thumb-y" tabindex="0" style="top: 0px; height: 300px;"></div>
-		</div>
-	</div>
-</div>
+
 
 </div>
