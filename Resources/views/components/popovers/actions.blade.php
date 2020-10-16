62902f8427121577afe7c1a6284130659e928b18
Malebestia <malebestia@yahoo.it>
2020-10-16 20:38:40 +0200

    .

--- Resources/views/components/popovers/actions.blade.php
+++ Resources/views/components/popovers/actions.blade.php
@@ -1,1 +1,14 @@
-<button type="button" class="btn btn-lg btn-danger" data-toggle="popover" title="{{ $title }}" data-content="{{ $content }}">aaa</button>
+@php
+    
+@endphp
+
+<button 
+    type="button" 
+    class="btn btn-lg btn-danger" 
+    data-toggle="popover" 
+    data-trigger="click" 
+    {{-- title="Popovertitle" --}}
+    data-content="And here's some amazing content. It's very engaging. Right?"
+    >
+    Click to toggle popover
+</button>
