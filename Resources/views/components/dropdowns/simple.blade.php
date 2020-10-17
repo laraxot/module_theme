d11915da92d94cdc422c64015afb3ba2a2bb15ca
Malebestia <malebestia@yahoo.it>
2020-10-17 21:30:41 +0200

    .

---
+++ Resources/views/components/dropdowns/simple.blade.php
@@ -0,0 +1,11 @@
+<div class="dropdown">
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown"
+        aria-haspopup="true" aria-expanded="false">
+        <i class="fas fa-cog"></i>
+    </button>
+    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+
+        {{ $btns }}
+
+    </div>
+</div>
