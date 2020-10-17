dad1a863b106744e71faacb7a46cb318c0ef6168
Malebestia <malebestia@yahoo.it>
2020-10-17 12:51:01 +0200

    .

--- Resources/views/components/popovers/actions.blade.php
+++ Resources/views/components/popovers/actions.blade.php
@@ -1,14 +1,25 @@
 @php
-    
+    //dddx(get_defined_vars());
+    extract($params);
 @endphp
 
 <button 
     type="button" 
-    class="btn btn-lg btn-danger" 
+    class="btn btn-sm btn-danger" 
     data-toggle="popover" 
-    data-trigger="click" 
-    {{-- title="Popovertitle" --}}
-    data-content="And here's some amazing content. It's very engaging. Right?"
+    {{-- data-trigger="click" --}}
+    data-html="true"
+    title="{{ $title }}"
+    data-content="
+        @foreach($actions as $action)
+            {!! $action->btnHtml(
+            [
+                'guest_notice'=>false,
+                'title' => true,
+                'error_label' => false,
+            ]) !!}
+        @endforeach
+        "
     >
-    Click to toggle popover
+    {{ $content_btn }}
 </button>
