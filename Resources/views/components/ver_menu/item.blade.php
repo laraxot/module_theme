d5c2626331ab1ea22647701f6a5aa42d473d1106
Malebestia <malebestia@yahoo.it>
2020-10-21 00:01:15 +0200

    .

--- Resources/views/components/ver_menu/item.blade.php
+++ Resources/views/components/ver_menu/item.blade.php
@@ -1,31 +1,33 @@
-<li class="menu-item menu-item-submenu {{ $menu_item_open ? 'menu-item-open':'' }}" aria-haspopup="true" data-menu-toggle="hover">
+<li class="menu-item menu-item-submenu menu-item-open{{-- $menu_item_open?'menu-item-open':'' --}}" aria-haspopup="true" data-menu-toggle="hover">
     <div class="menu-link menu-toggle">
         <span class="{{-- menu-linkmenu-toggle --}} svg-icon menu-icon">
             {{ $icon ?? '' }}
         </span>
-        <span class="menu-text" wire:click="showSubs({{ $parent }}-{{ $id }},'{{ $model_name }}')">{!! $title !!}</span>
+        <span class="menu-text" wire:click="showSubs('{{ $parent }}-{{ $id }}','{{ $model_name }}')">{!! $title !!} [{{ $parent }}-{{ $id }} {{ $model_name }}]</span>
         {!! $btns !!}
-        <i class="menu-arrow"></i>
+        <i class="menu-arrow" wire:click="showSubs('{{ $parent }}-{{ $id }}','{{ $model_name }}')"></i>
     </div>
     
         
     <div class="menu-submenu">
         <i class="menu-arrow"></i>
         <ul class="menu-subnav">
+            ---
             @foreach($sons as $son)
+                @php
+                    if(!isset($son['model_name'])){
+                        dddx([$son, 'item']);
+                    }
+                @endphp
                 @component('theme::components.ver_menu.item',[
-                            'sons'=>$son->sons ?? [],
-                            'model_name'=>$son->model_name,
+                            'sons'=>$son['sons'] ?? [],
+                            'model_name'=>$son['model_name'],
                             'btns' => '',
                             'menu_item_open' => '',
-                            'parent'=>$parent.'-'.$id
+                            'parent'=>$parent.'-'.$id,
+                            'id'=>$son['tree_id'],
+                            'title'=>$son['nome'].'['.$son['tree_id'].']',
                             ])
-                    @slot('title')
-                     {{ $son['nome'] }}   
-                    @endslot
-                    @slot('id')
-                     {{ $son['id'] }}   
-                    @endslot
                 @endcomponent
             @endforeach
         </ul>
