504ef97e86677de5f1a8538b4da8f8d13cc6ee33
marco76tv <marco.sottana@gmail.com>
2020-10-22 16:44:14 +0200

    .

--- Resources/views/components/ver_menu/item.blade.php
+++ Resources/views/components/ver_menu/item.blade.php
@@ -1,36 +1,46 @@
 <li class="menu-item menu-item-submenu menu-item-open{{-- $menu_item_open?'menu-item-open':'' --}}" aria-haspopup="true" data-menu-toggle="hover">
     <div class="menu-link menu-toggle">
         <span class="{{-- menu-linkmenu-toggle --}} svg-icon menu-icon">
-            {{ $icon ?? '' }}
+            {{ $icon ?? '['.substr($model_name,0,1).']' }}
         </span>
-        <span class="menu-text" wire:click="showSubs('{{ $parent }}-{{ $id }}','{{ $model_name }}')">{!! $title !!} [{{ $parent }}-{{ $id }} {{ $model_name }}]</span>
+        <span class="menu-text" wire:click="showSubs('{{ $parent }}-{{ $id }}','{{ $model_name }}')">{!! $title !!}</span>
         {!! $btns !!}
         <i class="menu-arrow" wire:click="showSubs('{{ $parent }}-{{ $id }}','{{ $model_name }}')"></i>
     </div>
-    
-        
+
+
     <div class="menu-submenu">
         <i class="menu-arrow"></i>
         <ul class="menu-subnav">
             @php
                 //dddx(['item',get_defined_vars()]);
             @endphp
-            @foreach($sons as $son)
+
+            @foreach($sons as $son_type)
+            @foreach($son_type as $son)
                 @php
                     if(!isset($son['model_name'])){
-                        dddx([$son, 'item']);
+                        dddx(['item','son'=>$son, 'sons'=>$sons,'son_type'=>$son_type]);
                     }
                 @endphp
                 @component('theme::components.ver_menu.item',[
                             'sons'=>$son['sons'] ?? [],
                             'model_name'=>$son['model_name'],
-                            'btns' => '',
                             'menu_item_open' => '',
                             'parent'=>$parent.'-'.$id,
-                            'id'=>$son['tree_id'],
-                            'title'=>$son['nome'].'['.$son['tree_id'].']',
+                            'id'=>$son['model_name'].'-'.$son['tree_id'],
+                            'title'=>$son['nome'],
                             ])
+                    @slot('btns')
+                    @component('theme::components.dropdowns.simple',[
+                        'btn_class'=>'btn btn-dark',
+                        'btns'=> $son['treeItemActions'] ?? '',
+                        ])
+                    @slot('title')<i class="fas fa-cog"></i>@endslot
+                    @endcomponent
+                @endslot
                 @endcomponent
+            @endforeach
             @endforeach
         </ul>
     </div>
