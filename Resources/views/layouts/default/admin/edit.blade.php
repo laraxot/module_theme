b260e656191ba627017b5cc3437476f5b61f3408
marco76tv <marco.sottana@gmail.com>
2020-10-26 21:18:32 +0100

    .

--- Resources/views/layouts/default/admin/edit.blade.php
+++ Resources/views/layouts/default/admin/edit.blade.php
@@ -1,16 +1,14 @@
 @extends('adm_theme::layouts.app')
+@php
+    $last_container=last($containers);
+@endphp
+
 @section('content')
-    @php
-    //ddd($item_view);
-    /*
-    ddd($item_view);
-    if(!\View::exists($view.'.form') && !\View::exists($view_default.'.form.'.$edit_type) ) {
-    ddd('non esiste ne ['.$view.'.form'.'] ne ['.$view_default.'.form.'.$edit_type.']');
-    }
-    */
-    @endphp
-    <div class="widget">
-        <div class="widget-body">
+    @component('theme::components.crud',get_defined_vars())
+        @slot('content')
+            {{--
+            @livewire('formx::edit',['model'=>$_panel->row])
+            --}}
             @if (count($errors) > 0)
                 <div class="alert alert-danger">
                     <ul>
@@ -21,10 +19,32 @@
                 </div>
             @endif
             @php
+                if(!is_object($row)) return '';
+                $fields=$_panel->editFields();
+            @endphp
+            {!! Form::bsOpenPanel($_panel, 'update') !!}
+            @foreach ($fields as $field)
+            {!! Theme::inputHtml(['row' => $row, 'field' => $field]) !!}
+            @endforeach
+            {{ Form::bsSubmit('Modifica') }}
+            {!! Form::close() !!}
+        @endslot
+    @endcomponent
+@endsection
+
+
+{{--
+@extends('adm_theme::layouts.app')
+@section('content')
+
+    <div class="widget">
+        <div class="widget-body">
+
+            @php
             if(!is_object($row)) return '';
             $fields=$_panel->editFields();
             //dddx(get_defined_vars());
-            //dddx($_panel);
+            //dddx($fields);
             @endphp
             {!! Form::bsOpenPanel($_panel, 'update') !!}
             <div class="row">
@@ -39,3 +59,4 @@
         </div>
     </div>
 @endsection
+--}}
