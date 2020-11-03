e4ba9749bd025ca55a015ab0673e177711cdc34f
Malebestia <malebestia@yahoo.it>
2020-11-03 20:22:37 +0100

    .

--- Resources/views/layouts/default/admin/create.blade.php
+++ Resources/views/layouts/default/admin/create.blade.php
@@ -1,5 +1,39 @@
 @extends('adm_theme::layouts.app')
 @section('content')
+    @component('theme::components.crud',get_defined_vars())
+        @slot('content')
+            {{--
+            @livewire('formx::create',['model'=>$_panel->row])
+            --}}
+            @if (count($errors) > 0)
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $k => $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+            @php
+                if(!is_object($row)) return '';
+                $fields=$_panel->editFields();
+            @endphp
+            {!! Form::bsOpenPanel($_panel, 'store') !!}
+            @foreach ($fields as $field)
+            {!! Theme::inputHtml(['row' => $row, 'field' => $field]) !!}
+            @endforeach
+            {{ Form::bsSubmit('Salva') }}
+            {!! Form::close() !!}
+        @endslot
+    @endcomponent
+@endsection
+
+
+
+
+
+{{-- 
+@section('content')
     @php
     //if(!\View::exists($view.'.form') && !\View::exists($view_default.'.form.'.$edit_type) ) {
     // ddd('non esiste ne ['.$view.'.form'.'] ne ['.$view_default.'.form.'.$edit_type.']');
@@ -31,3 +65,4 @@
         </div>
     </div>
 @endsection
+--}}
