9295713c9f6bfae04045829897bba8dc2c03c6ff
Malebestia <malebestia@yahoo.it>
2020-11-09 20:14:11 +0100

    .

--- Resources/views/layouts/default/admin/index_order.blade.php
+++ Resources/views/layouts/default/admin/index_order.blade.php
@@ -1,72 +1,21 @@
-@extends('pub_theme::layouts.app')
+@php
+    //dddx(get_defined_vars());
+    //Theme::add('https://cdn.jsdelivr.net/gh/livewire/sortable@v0.x.x/dist/livewire-sortable.js');
+@endphp
+@extends('adm_theme::layouts.app')
 @section('content')
 
-@include('theme::includes.flash')
-@include('theme::modal_ajax')
-<style>
-  	#sortable { list-style-type: none; margin: 0; padding: 0; width: 60%; }
-  	#sortable li { margin: 0 5px 5px 5px; padding: 5px; font-size: 1.2em; height: 1.5em; }
-  	html>body #sortable li { height: 1.5em; line-height: 1.2em; }
-  	.ui-state-highlight { height: 1.5em; line-height: 1.2em; }
-  	$('.ui-state-default').on('mousedown', function(){
-		$(this).css( 'cursor', 'move' );
-	}).on('mouseup', function(){
-		$(this).css( 'cursor', 'auto' );
-	});;
-
-</style>
-@php
-	Theme::add('theme/bc/jquery-ui/jquery-ui.min.js');
-	Theme::add('theme/bc/jquery-ui/themes/base/jquery-ui.min.css');
-	/*
-	https://itsolutionstuff.com/post/jquery-draggable-sortable-table-rows-example-demo-with-bootstrapexample.html
-	*/
-@endphp
-
-<div class="page-wrapper">
-	@include('pub_theme::layouts.partials.breadcrumb_params')
-	<ul id="sortable">
-	@foreach($rows as $row)
-		<li class="ui-state-default" id="{{ $row->post_id }}">
-			<span class="ui-icon ui-icon-arrowthick-2-n-s"></span>
-			[{{ $row->post_id }}][{{ $row->pivot->pos }}] {{ $row->title }}
-		</li>
-	@endforeach
-	</ol>
-</div>
-
-<form action="{{ Request::url() }}" method="POST">
-	@csrf
-	@method('POST')
-	<input type="hidden" name="order_list" value="{{$order_list}}"/>
-	<input type="submit" value="save" class="btn btn-primary"/>
-</form>
+@livewire('formx::crud.index_order', ['rows' => $rows])
+{{-- 
+    <ul>
+        @foreach($rows as $row)
+        
+        <li>
+            {{ $row->treeLabel() }}
+            
+        </li>
+        @endforeach
+    </ul>
+--}}
 
 @endsection
-
-@push('scripts')
-<script>
-	$(function() {
-		
-
-		$( "#sortable" ).sortable({
-			placeholder: "ui-state-highlight",
-			update: function (event, ui) {
-				var $list =  $(this).sortable("toArray").join("|");
-				//var sortable_data = $(this).sortable('serialize'); 
-				//alert(sortable_data);
-				$('input[name="order_list"]').val($list);
-				/*
-				 $.ajax({   url: "/persistListOrder.php",
-                                      data: { 
-                                                  'section':this.id,              
-                                                  'components': list
-                                            }
-                                  });
-                */
-			}
-		});
-		$( "#sortable" ).disableSelection();
-	});
-</script>
-@endpush
