117e6153074e5b08c0f8a80a52eab7ca73a6991e
Malebestia <malebestia@yahoo.it>
2020-11-17 22:54:01 +0100

    .

--- Resources/views/components/dropdowns/simple.blade.php
+++ Resources/views/components/dropdowns/simple.blade.php
@@ -1,7 +1,7 @@
 <div class="dropdown menu-label">
     <button class="{{ $btn_class ?? ''}}" type="button"  data-toggle="dropdown"
         aria-haspopup="true" aria-expanded="false">
-        {{ $title }}
+        {!! $title !!}
     </button>
     <div class="dropdown-menu" >
         {!! $btns !!}
--- Resources/views/components/ver_menu/item1.blade.php
+++ Resources/views/components/ver_menu/item1.blade.php
@@ -1,23 +1,58 @@
-<li class="{{ $li_class }}" {{ $li_attribute }}>
-    <a href="#" class="menu-link menu-toggle">
-        <span class="menu-text">{{ $node->row->treeLabel() }}   [{{ $node->id() }} ]</span>
-        <i class="menu-arrow"></i>
-	</a>
+<li class="{{ $li_class ?? ''}}" {{ $li_attribute ?? ''}}>
+    <span class="menu-link ">
+        <span class="svg-icon menu-icon">
+            {!! $icon !!}
+        </span>
+        <span class="menu-text menu-toggle">{{ $node->row->treeLabel() }} [{{ $node->id() }} ]</span>
     
-    <div class="menu-submenu">
-        <span class="menu-arrow"></span>
-        <ul class="menu-subnav">
-        @foreach($node->row->treeSons() as $sons)
-            @foreach($sons as $son)
-                @component('theme::components.ver_menu.item1',['node'=>Panel::get($son)->setParent($node), 'li_class' => 'menu-item  menu-item-parent'])
-                    
-                @endcomponent
-            @endforeach
+        @component('theme::components.dropdowns.simple',[
+            'btn_class'=>'btn btn-dark',
+            'title'=>'<i class="fas fa-cog"></i>',
+            ])
+        @slot('btns')
+         {!! $node->btnCrud(
+            ['title' => true, 'class' => 'dropdown-item', 'group' => false,'in_admin'=>$in_admin]
+        ) !!}
+        @foreach ($node->itemActions() as $action) 
+            {!! $action->btnHtml(['title' => true, 'class' => 'dropdown-item','in_admin'=>$in_admin]) !!}
         @endforeach
-        </ul>
+        @endslot
+        @endcomponent
+
+        @if($node->row->treeSonsCount())
+            <i class="menu-arrow  menu-toggle"></i>
+        @endif
+    </span>
+
+    <div class="menu-submenu ">
+        @if($node->row->treeSonsCount())
+            <i class="menu-arrow"></i>
+        @endif
+        @component('theme::components.ver_menu.container', ['ul_class' => 'menu-subnav'])
+            @slot('content')
+                @component('theme::components.lists.item', [
+                    'li_class' => 'menu-item  menu-item-parent', 
+                    'li_attribute'=> 'aria-haspopup="true"'
+                    ])
+                    @slot('content')
+                        <span class="menu-link">
+                            <span class="menu-text">{{ $node->row->treeLabel() }} [{{ $node->id() }} ]</span>
+                        </span>
+                    @endslot
+                @endcomponent
+                @foreach($node->row->treeSons() as $k_type=>$sons)
+                    @foreach($sons as $son)
+                        @component('theme::components.ver_menu.item1', [
+                            'li_class' => 'menu-item  menu-item-submenu', 
+                            'li_attribute'=> 'aria-haspopup="true" data-menu-toggle="hover"',
+                            'node' => Panel::get($son)->setParent($node),
+                            'icon'=> Theme::renderIcon(Theme::tenantConfig('icons.tree.'.$k_type)),
+                            'in_admin'=>$in_admin,
+                            ])
+                        @endcomponent
+                    @endforeach
+                @endforeach
+            @endslot
+        @endcomponent
     </div>
 </li>
-
-
-
-
