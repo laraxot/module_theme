494f47f87e17ed13ceb763047d543b7b663212e9
marco76tv <marco.sottana@gmail.com>
2021-01-03 21:12:52 +0100

    .

--- Resources/lang/it/txt.php
+++ Resources/lang/it/txt.php
@@ -1,41 +1,29 @@
-<?php
-
-return [
-    'day_name' => [
-        0 => 'Domenica',
-        1 => 'Lunedì',
-        2 => 'Martedì',
-        3 => 'Mercoledì',
-        4 => 'Giovedì',
-        5 => 'Venerdì',
-        6 => 'Sabato',
-    ],
-
-    'sort' => [
-        'result' => 'risultati trovati',
-        'sort_by' => 'Ordina per',
-        'distance|asc' => 'Distanza',
-        'ratings_avg|desc' => 'Valutazione',
-        'ratings_count|desc' => 'Numero valutazioni',
-        'updated_at|desc' => 'Creazione',
-    ],
-
-    'search' => [
-        'keyword' => 'Parola chiave',
-        'city' => 'Città',
-        'category' => 'Categoria',
-        'cuisine' => 'Piatti',
-        'more_filter' => 'Aggiungi filtri',
-        'less_filter' => 'Togli filtri',
-        'filter' => 'Filtra',
-    ],
-
-    'rating' => 'Valutazione',
-    'show' => 'Visualizza',
-    'edit' => 'Edita',
-    'search' => 'Cerca',
-    'index_edit' => 'Gestisci',
-    'index' => 'Visualizza',
-    'close' => 'Chiudi',
-    'add_new' => 'Aggiungi',
-];
+<?php return array (
+  'day_name' => 
+  array (
+    0 => 'Domenica',
+    1 => 'Lunedì',
+    2 => 'Martedì',
+    3 => 'Mercoledì',
+    4 => 'Giovedì',
+    5 => 'Venerdì',
+    6 => 'Sabato',
+  ),
+  'sort' => 
+  array (
+    'result' => 'risultati trovati',
+    'sort_by' => 'Ordina per',
+    'distance|asc' => 'Distanza',
+    'ratings_avg|desc' => 'Valutazione',
+    'ratings_count|desc' => 'Numero valutazioni',
+    'updated_at|desc' => 'Creazione',
+  ),
+  'search' => 'Cerca',
+  'rating' => 'Valutazione',
+  'show' => 'Visualizza',
+  'edit' => 'Edita',
+  'index_edit' => 'Gestisci',
+  'index' => 'Visualizza',
+  'close' => 'Chiudi',
+  'add_new' => 'Aggiungi',
+);
