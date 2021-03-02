9296888841b1337b83ddaa03d4e9206d51ff0840
marco76tv <marco.sottana@gmail.com>
2021-03-02 19:49:16 +0100

    jet

---
+++ Http/Livewire/NavigationMenu.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Modules\Theme\Http\Livewire;
+
+use Livewire\Component;
+
+class NavigationMenu extends Component
+{
+    /**
+     * The component's listeners.
+     *
+     * @var array
+     */
+    protected $listeners = [
+        'refresh-navigation-menu' => '$refresh',
+    ];
+
+    /**
+     * Render the component.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function render()
+    {
+        return view('navigation-menu');
+    }
+}
