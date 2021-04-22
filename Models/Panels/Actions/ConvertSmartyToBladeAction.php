f94732c31733fc18f5f1638f07937f4d7195199a
Malebestia <malebestia@yahoo.it>
2021-04-23 00:06:15 +0200

    .

--- Models/Panels/Actions/ConvertSmartyToBladeAction.php
+++ Models/Panels/Actions/ConvertSmartyToBladeAction.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 
 namespace Modules\Theme\Models\Panels\Actions;
 
+use Illuminate\Support\Facades\File;
+use Modules\Ewb\Services\EwbService;
 use Modules\Xot\Models\Panels\Actions\XotBasePanelAction;
+use Modules\Xot\Services\FileService;
+use Modules\Xot\Services\SmartyService;
 
 /**
  * Class TestAction.
@@ -12,11 +16,32 @@ use Modules\Xot\Models\Panels\Actions\XotBasePanelAction;
 class ConvertSmartyToBladeAction extends XotBasePanelAction {
     public bool $onContainer = true;
 
-    //public string $icon = '<i class="fa fa-edit"></i>';
+    public string $icon = '<i class="fas fa-exchange-alt"></i>';
 
     /**
      * @return mixed
      */
     public function handle() {
+        $converted = EwbService::convertSmartyToBladeAction();
+
+        /*
+        $directory = FileService::getViewNameSpacePath('pub_theme');
+
+        $files = File::allFiles($directory);
+        $smarty = new SmartyService();
+
+        foreach ($files as $file) {
+            //dddx(get_class_methods($file));
+            if ('tpl' == $file->getExtension()) {
+                $converted = $smarty->convert($file->getRealPath());
+
+
+                $new_file = substr($file->getRealPath(), 0, -3).'blade.php';
+                //dddx($new_file);
+                File::put($new_file, $converted);
+            }
+        }
+        */
+        return '<h3>+Done</h3>';
     }
 }
