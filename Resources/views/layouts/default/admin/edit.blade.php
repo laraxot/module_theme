5ff1a62c922562c841134cc2eb92551b85f0162f
Malebestia <malebestia@yahoo.it>
2021-06-18 20:40:18 +0200

    .

--- Resources/views/layouts/default/admin/edit.blade.php
+++ Resources/views/layouts/default/admin/edit.blade.php
@@ -1,18 +1,14 @@
 @extends('adm_theme::layouts.app')
 @php
-    $last_container=last($containers);
+$last_container = last($containers);
 @endphp
 
 @section('content')
-    @component('theme::components.crud',get_defined_vars())
+    @component('theme::components.crud', get_defined_vars())
         @slot('content')
-            {!! Theme::include('topbar',[],get_defined_vars()) !!}
-            {{--
-            {!! Theme::include('tabs',['tabs'=>$_panel->getTabs()],get_defined_vars()) !!}
-            --}}
-            {{--
-            @livewire('formx::edit',['model'=>$_panel->row])
-            --}}
+            {!! Theme::include('topbar', [], get_defined_vars()) !!}
+            {{-- {!! Theme::include('tabs',['tabs'=>$_panel->getTabs()],get_defined_vars()) !!} --}}
+            {{-- @livewire('formx::edit',['model'=>$_panel->row]) --}}
             @if (count($errors) > 0)
                 <div class="alert alert-danger">
                     <ul>
@@ -23,12 +19,14 @@
                 </div>
             @endif
             @php
-                if(!is_object($row)) return '';
-                $fields=$_panel->editFields();
+            if (!is_object($row)) {
+                return '';
+            }
+            $fields = $_panel->editFields();
             @endphp
             {!! Form::bsOpenPanel($_panel, 'update') !!}
             @foreach ($fields as $field)
-            {!! Theme::inputHtml(['row' => $row, 'field' => $field]) !!}
+                {!! Theme::inputHtml(['row' => $row, 'field' => $field]) !!}
             @endforeach
             {{ Form::bsSubmit('Modifica') }}
             {!! Form::close() !!}
@@ -37,8 +35,7 @@
 @endsection
 
 
-{{--
-@extends('adm_theme::layouts.app')
+{{-- @extends('adm_theme::layouts.app')
 @section('content')
 
     <div class="widget">
@@ -62,5 +59,4 @@
 
         </div>
     </div>
-@endsection
---}}
+@endsection --}}
