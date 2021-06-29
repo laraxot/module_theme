4b88e97a7b636c8d2f9f67121aa79342809e9f19
marco76tv <marco.sottana@gmail.com>
2021-06-29 09:35:54 +0200

    .

--- Providers/ThemeServiceProvider.php
+++ Providers/ThemeServiceProvider.php
@@ -57,7 +57,7 @@ class ThemeServiceProvider extends XotBaseServiceProvider {
             \Modules\Theme\Console\CreateThemeCommand::class,
         ]);
         //dddx(dirname($this->module_ns));
-        Blade::componentNamespace(dirname($this->module_ns).'\View\Components', 'theme');
+        Blade::componentNamespace('Modules\Theme\View\Components', 'theme');
     }
 
     public function registerCallback(): void {
