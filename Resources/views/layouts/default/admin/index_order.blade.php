6b5bcbcb1f4f53a1331f0b2dd592a6c8b4af4791
Malebestia <malebestia@yahoo.it>
2021-06-29 20:17:59 +0200

    .

--- Resources/views/layouts/default/admin/index_order.blade.php
+++ Resources/views/layouts/default/admin/index_order.blade.php
@@ -1,42 +1,38 @@
 @php
-    //dddx(get_defined_vars());
-    //Theme::add('https://cdn.jsdelivr.net/gh/livewire/sortable@v0.x.x/dist/livewire-sortable.js');
+//dddx(get_defined_vars());
+//Theme::add('https://cdn.jsdelivr.net/gh/livewire/sortable@v0.x.x/dist/livewire-sortable.js');
 @endphp
 @extends('adm_theme::layouts.app')
 @section('content')
 
 
 
-@if(isset($params['module']))
-    @php
-        $view_index_order = $params['module'].'::livewire.index_order';
-        //dddx($view_index_order);
-    @endphp
-    @if(view()->exists($view_index_order))
-        @livewire($params['module'].'::crud.index_order', ['rows' => $rows])
+    @if (isset($params['module']))
+        @php
+            $view_index_order = $params['module'] . '::livewire.index_order';
+            //dddx($view_index_order);
+        @endphp
+        @if (view()->exists($view_index_order))
+            @livewire($params['module'].'::crud.index_order', ['rows' => $rows])
+        @else
+
+            NOT EXISTS {{ $view_index_order }}
+            {{-- @livewire('formx::crud.index_order', ['rows' => $rows]) --}}
+            @livewire('ewb::crud.index_order', ['rows' => $rows]) {{-- questa riga è sbagliata --}}
+        @endif
     @else
-          
-        NOT EXISTS {{ $view_index_order }}
-        {{--
         @livewire('formx::crud.index_order', ['rows' => $rows])
-        --}}
-        @livewire('ewb::crud.index_order', ['rows' => $rows])
     @endif
-@else
-    @livewire('formx::crud.index_order', ['rows' => $rows])
-@endif
 
 
-{{-- 
-    <ul>
-        @foreach($rows as $row)
+    {{-- <ul>
+        @foreach ($rows as $row)
         
         <li>
             {{ $row->treeLabel() }}
             
         </li>
         @endforeach
-    </ul>
---}}
+    </ul> --}}
 
 @endsection
