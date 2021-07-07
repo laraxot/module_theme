01225e68aa944e015848b636e6cd56999caf545d
marco76tv <marco.sottana@gmail.com>
2021-07-07 11:07:42 +0200

    .

--- Models/Panels/Actions/TestAction.php
+++ Models/Panels/Actions/TestAction.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Modules\Theme\Models\Panels\Actions;
 
+use Intervention\Image\Facades\Image;
 use Modules\Xot\Models\Panels\Actions\XotBasePanelAction;
-use Modules\Xot\Services\SmartyService;
+
+//use Modules\Xot\Services\SmartyService;
 
 /**
  * Class TestAction.
@@ -20,7 +22,24 @@ class TestAction extends XotBasePanelAction {
      */
     public function handle() {
         //return $this->panel->view();
+        /*
         $smarty = new SmartyService();
         $smarty->convert(storage_path('test3.tpl'));
+        */
+
+        $img = Image::cache(function ($image) {
+            $img_src = asset('img/test00.jpg');
+            //dddx($img_src);//http://ptvx.local/img/test00.jpg
+
+            return $image->make($img_src)->resize(300, 200)->greyscale();
+        }, 60 * 60 * 24, true);
+
+        dddx([
+            'getEncoded' => $img->getEncoded(),
+            'basePath' => $img->basePath(),
+            'methods' => get_class_methods($img),
+        ]);
+
+        return 'preso';
     }
 }
