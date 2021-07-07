3672b00898876e6495e2fc65152d0903114129e3
marco76tv <marco.sottana@gmail.com>
2021-07-07 13:44:45 +0200

    .

--- Resources/views/layouts/default/create/item.blade.php
+++ Resources/views/layouts/default/create/item.blade.php
@@ -1,10 +1,13 @@
 @php
-	if(!is_object($row)) return '';
-	$fields=$_panel->createFields();
+if (!is_object($row)) {
+    return '';
+}
+$fields = $_panel->createFields();
 @endphp
-{!! Form::bsOpen($row,'store') !!}
-@foreach($fields as $field)
-	{!! Theme::inputHtml(['row'=>$row,'field'=>$field]) !!}
+{!! Form::bsOpen($row, 'store') !!}
+@foreach ($fields as $field)
+    {!! Theme::inputHtml(['row' => $row, 'field' => $field]) !!}
 @endforeach
-{{ $_panel->btnSubmit() }}
+{{ Form::bsSubmit() }}
+{{-- $_panel->btnSubmit() --}}
 {!! Form::close() !!}
