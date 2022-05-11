b9e945a03699e462e83518bbe7a4649829edbe86
marco xot <marco.sottana@gmail.com>
2022-05-11 18:17:01 +0200

    up

--- Resources/views/components/input/search/field.blade.php
+++ Resources/views/components/input/search/field.blade.php
@@ -1,7 +1,14 @@
 <div class="input-group mb-3">
     <input class="form-control bg-transparent border-dark border-end-0" type="search" placeholder="Search here"
-        aria-label="Search here" wire:model="q" >
-    <button class="btn btn-outline-dark border-start-0" wire:click="search()">
-        <i class="{{ $attrs['icon'] ?? 'fa fa-search' }} text-lg"></i>
+        aria-label="Search here" wire:model.defer="q" >
+    <button class="btn btn-outline-dark border-start-0" wire:click="search()" >
+        <div wire:loading wire:target="search">
+            <div class="spinner-border text-success" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+        </div>
+        <div wire:loading.remove wire:target="search">
+            <i class="fa fa-search text-lg"></i>
+        </div>
     </button>
 </div>
