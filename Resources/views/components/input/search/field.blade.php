0a783cd1324ea8a9738a11f0409bc2fb1340ff04
marco76tv <marco.sottana@gmail.com>
2022-05-11 14:17:02 +0200

    .

---
+++ Resources/views/components/input/search/field.blade.php
@@ -0,0 +1,7 @@
+<div class="input-group mb-3">
+    <input class="form-control bg-transparent border-dark border-end-0" type="search" placeholder="Search here"
+        aria-label="Search here" wire:model="q" >
+    <button class="btn btn-outline-dark border-start-0" wire:click="search()">
+        <i class="{{ $attrs['icon'] ?? 'fa fa-search' }} text-lg"></i>
+    </button>
+</div>
