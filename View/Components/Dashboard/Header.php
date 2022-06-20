00cb6552aafbffb40c02d9c307005d87fd118e60
Malebestia <malebestia@yahoo.it>
2022-06-20 17:43:06 +0200

    .

---
+++ View/Components/Dashboard/Header.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Theme\View\Components\Dashboard;
+
+use Illuminate\View\Component;
+
+class Header extends Component {
+    public string $view;
+    public string $title;
+    public string $subtitle;
+
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct(
+        string $view, // esempio 'adm_theme::components.dashboard.header'
+        string $title,
+        string $subtitle = '') {
+        $this->view = $view;
+        $this->title = $title;
+        $this->subtitle = $subtitle;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function render() {
+        $view = $this->view;
+        $view_params = [
+            'view' => $this->view,
+        ];
+
+        return view()->make($view, $view_params);
+    }
+}
