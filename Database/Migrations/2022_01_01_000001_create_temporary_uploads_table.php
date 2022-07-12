6ce9aaf4643d9594bedee8edfcd1e9168ba90784
marco xot <marco.sottana@gmail.com>
2022-07-12 12:13:01 +0200

    .

---
+++ Database/Migrations/2022_01_01_000001_create_temporary_uploads_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+use Modules\Xot\Database\Migrations\XotBaseMigration;
+
+class CreateTemporaryUploadsTable extends XotBaseMigration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        //-- CREATE --
+        $this->tableCreate(
+            function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('session_id');
+                $table->timestamps();
+            }
+        );
+        //-- UPDATE --
+        $this->tableUpdate(
+            function (Blueprint $table) {
+            if (! $this->hasColumn('updated_at')) {
+                $table->timestamps();
+            }
+            if (! $this->hasColumn('updated_by')) {
+                $table->string('updated_by')->nullable()->after('updated_at');
+                $table->string('created_by')->nullable()->after('created_at');
+            }
+        }); //end update
+    }
+}
