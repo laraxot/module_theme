a829445eda326b08c3aa08ad6dcfeae9b1bcb5c2
marco xot <marco.sottana@gmail.com>
2022-09-05 10:36:39 +0200

    up

---
+++ Resources/views/components/progress/bar_tot.blade.php
@@ -0,0 +1,7 @@
+{{ $value }}/{{$tot}}
+<div class="progress progress-striped active" style="height:5px;">
+    <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="60" aria-valuemin="0"
+        aria-valuemax="100" style="width: {{ ($value*100)/$tot }}%;">
+        <span class="sr-only">{{ $value }}% Complete (success)</span>
+    </div>
+</div>
---
+++ View/Components/Progress/Tot/Bar.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Theme\View\Components\Progress\Tot;
+
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\View\Component;
+
+/**
+ * Class Bar.
+ */
+class Bar extends Component {
+    public float $value;
+    public float $tot;
+
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct(float $value, float $tot) {
+        $this->value = $value;
+        $this->tot = $tot;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): Renderable {
+        /** 
+        * @phpstan-var view-string
+        */
+        $view = 'theme::components.progress.bar_tot';
+        $view_params = [
+            'view' => $view,
+        ];
+
+        return view()->make($view, $view_params);
+    }
+}
