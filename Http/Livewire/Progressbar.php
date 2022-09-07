66d7228ef7e53247751b98fc662d19a5cb15ad72
marco xot <marco.sottana@gmail.com>
2022-09-07 19:48:52 +0200

    up

---
+++ Http/Livewire/Progressbar.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Theme\Http\Livewire;
+
+use Illuminate\Contracts\Support\Renderable;
+use Livewire\Component;
+
+class Progressbar extends Component {
+    public int $perc = 0;
+    public int $loop_index = 0;
+    public int $loop_max = 100;
+    
+
+    /**
+     * Undocumented function.
+     */
+    public function render(): Renderable {
+        $view = 'theme::livewire.progressbar.v1';
+        $view_params = [
+            'view' => $view,
+        ];
+
+        return view($view, $view_params);
+    }
+
+    /**
+     * --.
+     */
+    public function start(): void {
+        if ($this->loop_index < $this->loop_max) {
+            $this->perc = (int) ($this->loop_index * 100/$this->loop_max);
+            $this->handle();
+            $this->emit('updateProgress', $this->perc);
+        } else {
+            session()->flash('message', 'FINITO');
+        }
+    }
+
+    public function handle(): void {
+        ++$this->loop_index;
+    }
+}
---
+++ Resources/views/livewire/progressbar/v1.blade.php
@@ -0,0 +1,20 @@
+<div>
+    <div>
+        @if (session()->has('message'))
+            <div class="alert alert-success">
+                {{ session('message') }}
+            </div>
+        @endif
+    </div>
+    {{$loop_index}}/{{$loop_max}}
+    <x-progress.bar value="{{ $perc }}" />
+    <br/>
+    <button wire:click="start()" class="btn btn-success">Start</button>
+    @push('scripts')
+    <script>
+        Livewire.on('updateProgress', perc => {
+            @this.start();
+        });
+    </script>
+    @endpush
+</div>
