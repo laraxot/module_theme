c66e6db1e03b482fb265833db52971edbf8c7f8b
marco xot <marco.sottana@gmail.com>
2022-09-09 10:15:39 +0000

    up

--- Resources/views/livewire/input/arr/elastic/filter.blade.php
+++ Resources/views/livewire/input/arr/elastic/filter.blade.php
@@ -31,11 +31,22 @@
     </div>
     @endforeach
 
+    <div class="input-group mb-3">
+        
+        {{--<x-input.date name="dateFrom" class="form-control" placeholder="dateFrom" :value="date('Y-m-d')"></x-input.date>
+        <x-input.date name="dateTo" class="form-control" placeholder="dateTo" :value="date('Y-m-d')"></x-input.date>--}}
+        <input class="form-control" type="date" id="dateFrom" name="dateFrom">
+        <input class="form-control" type="date" id="dateTo" name="dateTo">
+    </div>
     {{--
-    <div class="form-group">
-        <x-calendar></x-calendar>
+        https://bootstrap-datepicker.readthedocs.io/en/latest/markup.html#date-range
+    <div class="input-group input-daterange">
+        <input type="text" class="form-control" value="2012-04-05">
+        <div class="input-group-addon">to</div>
+        <input type="text" class="form-control" value="2012-04-19">
     </div>
     --}}
+    
 
 
     <div class="form-group">
@@ -49,9 +60,9 @@
 </div>
 @push('scripts')
     <script>
-        Date.prototype.oneYearAgo = (function() {
+        Date.prototype.oneMonthAgo = (function() {
             var local = new Date(this);
-            local.setFullYear(this.getFullYear() - 1);
+            local.setMonth(this.getMonth() - 1);
             return local.toJSON().slice(0, 10);
         });
         document.getElementById('dateFrom').value = new Date().oneYearAgo();
---
+++ View/Components/Input/Date.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Theme\View\Components\Input;
+
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\View\Component;
+use Modules\Xot\Services\FileService;
+
+/**
+ * Undocumented class.
+ */
+class Date extends Component {
+    public array $attrs = [];
+
+    /**
+     * Undocumented function.
+     */
+    public function __construct(?string $class, string $placeholder, string $name, ?string $style = '', string $value = '') {
+        $this->attrs['class'] = $class;
+        $this->attrs['style'] = $style;
+        $this->attrs['placeholder'] = $placeholder;
+        $this->attrs['name'] = $name;
+        $this->attrs['value'] = $value;
+    }
+
+    /**
+     * Get the view / contents that represents the component.
+     */
+    public function render(): Renderable {
+        $theme = inAdmin() ? 'adm_theme' : 'pub_theme';
+        FileService::viewCopy('theme::components.input.date', $theme.'::components.input.date');
+
+        $view = $theme.'::components.input.date';
+
+        $view_params = [
+            'view' => $view,
+        ];
+
+        return view()->make($view, $view_params);
+    }
+}
