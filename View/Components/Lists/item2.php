16ea0ea244cf02d635e27d815aa07e4015dee96c
Malebestia <malebestia@yahoo.it>
2022-09-09 17:44:31 +0200

    .

---
+++ View/Components/Lists/item2.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Theme\View\Components\Lists;
+
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\View\Component;
+
+/**
+ * Class Item2.
+ */
+class Item2 extends Component {
+    public ?string $li_class;
+    public ?string $li_attribute;
+
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct(?string $li_class = '', ?string $li_attribute = '') {
+        $this->li_class = $li_class;
+        $this->li_attribute = $li_attribute;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): Renderable {
+        /**
+         * @phpstan-var view-string
+         */
+        $view = 'theme::components.lists.item';
+
+        $view_params = [
+            'view' => $view,
+        ];
+
+        return view()->make($view, $view_params);
+    }
+}
