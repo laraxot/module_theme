8ca7a9d300f85102bb2c6c54d35849e2dd0b2b2c
marco76tv <marco.sottana@gmail.com>
2022-09-13 11:09:17 +0200

    up

--- View/Components/Card.php
+++ View/Components/Card.php
@@ -5,81 +5,24 @@ declare(strict_types=1);
 namespace Modules\Theme\View\Components;
 
 use Illuminate\Contracts\Support\Renderable;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\View;
 use Illuminate\View\Component;
 
 class Card extends Component {
-    // public string $bg;
-    public ?string $title;
-    public ?string $type;
-
-    public ?Model $model;
-
-    public ?string $avatar = null;
-    public ?string $category = null;
-    public ?string $src = null;
-
-    public bool $collapsed;
-    public bool $removable;
-    public bool $maximizable;
-    public bool $disabled;
-    public bool $outline;
-    public bool $full;
+    public ?string $type = 'card';
 
     public array $attrs = [];
 
-    public function __construct(
-        string $bg = 'info',
-        ?string $title = null,
-        ?string $type = null,
-        ?Model $model = null,
-        ?string $avatar = null,
-        ?string $category = null,
-        bool $collapsed = false,
-        bool $removable = false,
-        bool $maximizable = false,
-        bool $disabled = false,
-        bool $outline = false,
-        bool $full = false,
-        ?string $src = '',
-    ) {
-        // $this->bg = $bg;
-        $this->model = $model;
-
-        $this->title = $title;
-
-        $this->type = $type;
-        $this->avatar = $avatar;
-        $this->category = $category;
-
-        $this->collapsed = $collapsed;
-        $this->removable = $removable;
-        $this->maximizable = $maximizable;
-        $this->disabled = $disabled;
-        $this->outline = $outline;
-        $this->full = $full;
-
-        $this->attrs['class'] =
-        'card card-'.$bg.
-         ($outline ? ' card-outline' : '').
-         ($collapsed ? ' collapsed-card' : '').
-         ($full ? ' bg-'.$bg : '');
-
-        $this->src = $src;
+    public function __construct(?string $type = null) {
+        $this->type = $type ?? 'card';
     }
 
     public function render(): Renderable {
         /**
-         * var view-string|null.
-         */
-        /**
          * @phpstan-var view-string
          */
-        $view = 'theme::components.card';
-        if (null !== $this->type) {
-            $view .= '.'.$this->type;
-        }
+        $view = 'theme::components.card.'.$this->type;
+
         $view_params = [];
 
         return View::make($view, $view_params);
