11e76c57fd01a3bda2adde70c2ef2e6cd37eadd0
marco xot <marco.sottana@gmail.com>
2022-09-24 18:34:21 +0200

    up

--- View/Components/Lists/Container.php
+++ View/Components/Lists/Container.php
@@ -11,15 +11,16 @@ use Illuminate\View\Component;
  * Class Container.
  */
 class Container extends Component {
-    public ?string $ul_class;
+    public ?string $ul_class = 'xxx';
 
     /**
      * Create a new component instance.
      *
      * @return void
      */
-    public function __construct(?string $ul_class = '') {
+    public function __construct(?string $ul_class = 'xxx') {
         $this->ul_class = $ul_class;
+        dddx([$ul_class, $this->ul_class]);
     }
 
     /**
