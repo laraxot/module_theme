32e191bf01c3c790805b7e3cc09a4780ab7831aa
Malebestia <malebestia@yahoo.it>
2022-10-07 13:04:51 +0200

    .

--- Resources/views/components/button/text.blade.php
+++ Resources/views/components/button/text.blade.php
@@ -1,1 +1,23 @@
-##TEXT##
+@props([
+    'disservizio_btn',
+])
+{{ dddx($disservizio_btn) }}
+
+<div class="cmp-text-button @if($disservizioBtn) mt-0 @endif @if($sBtn) mb-40 mb-lg-80 mt-lg-40 @endif {{$textBtnClass}}">
+    @if($cardDescription)
+     <h2 class="title-xxlarge mb-0">{{$cardTitle}}</h2>
+     <div class="text-wrapper">
+       <p class="subtitle-small mb-3 {{$subtitle_class}}">{{$cardDescription}}</p>
+     </div>
+     @endif
+     <div class="button-wrapper {{$class}}">
+        {{-- {{> @partial-block}} --}}
+        {!! $slot !!}
+     </div>
+  
+
+     @if($linkLabel)
+      <a class="{{$classLink}} simple-link" href="#">{{$linkLabel}} <span class="visually-hidden">{{$linkLabel}}</span></a>
+      @endif
+   </div>
+  
