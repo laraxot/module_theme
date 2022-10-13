2e96ad6daeae6a8c10e984476fc8a36aa4f92f09
marco xot <marco.sottana@gmail.com>
2022-10-13 16:06:28 +0200

    up

--- Resources/views/components/list/rows/category.blade.php
+++ Resources/views/components/list/rows/category.blade.php
@@ -21,18 +21,18 @@
   @endforeach
 @else
 
-@foreach($rows as $filter)
+
   <fieldset>
     <div class="categoy-list pb-4">
-      <legend class="h6 text-uppercase category-list__title">{{$filter->title}}</legend>
+      <legend class="h6 text-uppercase category-list__title">{{$title ?? 'Categoria'}}</legend>
       <ul>
 
-        @foreach($filter->list as $checkbox)
+        @foreach($rows as $checkbox)
           <li>
             <div class="form-check">
               <div class="checkbox-body border-light py-3">
                 <input type="checkbox" id="{{$checkbox->id}}" name="{{$checkbox->name}}" value="{{$checkbox->value}}">
-                <label for="{{$checkbox->id}}" class="subtitle-small_semi-bold mb-0 category-list__list">{{$checkbox->label}}</label> 
+                <label for="{{$checkbox->id}}" class="subtitle-small_semi-bold mb-0 category-list__list">{{$checkbox->name}}</label> 
               </div>
             </div>
           </li>
@@ -40,5 +40,5 @@
       </ul>
     </div>
   </fieldset>
-@endforeach
+
 @endif
