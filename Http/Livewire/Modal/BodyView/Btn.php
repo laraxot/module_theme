421ca8d1c9a6b22022b6a370654d4fc8b2981f4e
Malebestia <malebestia@yahoo.it>
2022-10-14 18:36:15 +0200

    .

---
+++ Http/Livewire/Modal/BodyView/Btn.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Theme\Http\Livewire\Modal\BodyView;
+
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Support\Collection;
+use Modules\Theme\Http\Livewire\Modal\BodyView as BaseComponent;
+
+/**
+ * Class Sort/Collection.
+ */
+class Btn extends BaseComponent {
+    public function render(): Renderable {
+        /**
+         * @phpstan-var view-string
+         */
+        $view = 'theme::livewire.modal.body_view.btn';
+
+        $view_params = [
+            'view' => $view,
+        ];
+
+        return view()->make($view, $view_params);
+    }
+}
---
+++ Resources/views/livewire/modal/body_view/btn.blade.php
@@ -0,0 +1,7 @@
+<div>
+@push('modals')
+    @include('theme::livewire.modal.body_view')
+@endpush
+<button class="btn btn-primary" wire:click="showModal"> aaaa </button>
+
+</div>
