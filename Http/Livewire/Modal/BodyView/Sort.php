50d9ddcb71f8cf386eaadc3f0b089094bbe7accb
marco76tv <marco.sottana@gmail.com>
2022-10-17 09:35:57 +0200

    rebase

--- Http/Livewire/Modal/BodyView/Sort.php
+++ Http/Livewire/Modal/BodyView/Sort.php
@@ -4,14 +4,38 @@ declare(strict_types=1);
 
 namespace Modules\Theme\Http\Livewire\Modal\BodyView;
 
+<<<<<<< HEAD
 use Illuminate\Support\Collection;
+=======
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Support\Collection;
+use Livewire\Component;
+use Modules\Xot\Contracts\PanelContract;
+use Modules\Xot\Services\PanelService;
+
+>>>>>>> bbaedf0 (.)
 use Modules\Theme\Http\Livewire\Modal\BodyView as BaseComponent;
 
 /**
  * Class Sort/Collection.
+<<<<<<< HEAD
  */
 class Sort extends BaseComponent {
     public function updateTaskOrder(array $list): void {
         dddx($list);
     }
 }
+=======
+ *
+ */
+class Sort extends BaseComponent {
+    
+    /**
+     * 
+     */
+    public function updateTaskOrder(array $list):void {
+        dddx($list);
+    }
+
+}
+>>>>>>> bbaedf0 (.)
--- Http/Livewire/Sort/Coll.php
+++ Http/Livewire/Sort/Coll.php
@@ -7,14 +7,24 @@ namespace Modules\Theme\Http\Livewire\Sort;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Support\Collection;
 use Livewire\Component;
+<<<<<<< HEAD
 
 /**
  * Class Sort/Collection.
+=======
+use Modules\Xot\Contracts\PanelContract;
+use Modules\Xot\Services\PanelService;
+
+/**
+ * Class Sort/Collection.
+ *
+>>>>>>> bbaedf0 (.)
  */
 class Coll extends Component {
     public Collection $data;
 
     /**
+<<<<<<< HEAD
      * Undocumented function.
      *
      * @param Collection $data
@@ -22,10 +32,19 @@ class Coll extends Component {
      * @return void
      */
     public function mount($data) {
+=======
+     * Undocumented function
+     *
+     * @param [type] $data
+     * @return void
+     */
+    public function mount($data){ 
+>>>>>>> bbaedf0 (.)
         $this->data = $data;
     }
 
     /**
+<<<<<<< HEAD
      * Undocumented function.
      */
     public function render(): Renderable {
@@ -38,3 +57,14 @@ class Coll extends Component {
         return view($view, $view_params);
     }
 }
+=======
+     * Undocumented function
+     *
+     * @return Renderable
+     */
+    public function render():Renderable{
+        return view('theme::livewire.sort.coll');
+    }
+
+}
+>>>>>>> bbaedf0 (.)
