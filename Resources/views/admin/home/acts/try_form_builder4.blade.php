f685ed4138380e6839c967f881632387a69583f3
Malebestia <malebestia@yahoo.it>
2022-10-19 18:43:43 +0200

    up

--- Resources/views/admin/home/acts/try_form_builder4.blade.php
+++ Resources/views/admin/home/acts/try_form_builder4.blade.php
@@ -1,5 +1,24 @@
 @extends('adm_theme::layouts.app')
 @section('content')
     <h3>Form Builder </h3>
-    <livewire:form.builder.v4 />
+    {{-- <livewire:form.builder.v4 /> --}}
+
+    <div class="build-wrap form-wrapper-div"></div>
 @endsection
+
+@push('styles')
+    <style>
+        .form-wrapper-div {
+            padding: 20px;
+        }
+    </style>
+@endpush
+@push('scripts')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+    <script src="https://formbuilder.online/assets/js/form-builder.min.js"></script>
+    <script>
+        $(function() {
+            $('.build-wrap').formBuilder();
+        });
+    </script>
+@endpush
