398570aa8128bed791a9706372424c99dfe4a9f1
marco xot <marco.sottana@gmail.com>
2022-10-21 10:35:22 +0200

    .

--- Resources/views/collective/fields/wysiwyg/sceditor/field.blade.php
+++ Resources/views/collective/fields/wysiwyg/sceditor/field.blade.php
@@ -1,14 +1,14 @@
 @php
-// https://github.com/samclarke/SCEditor
-// https://www.sceditor.com/
-
-Theme::add('https://cdn.jsdelivr.net/npm/sceditor@3/minified/themes/default.min.css');
-Theme::add('https://cdn.jsdelivr.net/npm/sceditor@3/minified/sceditor.min.js');
-Theme::add('https://cdn.jsdelivr.net/npm/sceditor@3/minified/formats/xhtml.min.js');
-
-$field = transFields(get_defined_vars());
-$field->attributes['style'] = 'height:300px;width:100%;';
-$field->attributes['id'] = $name;
+    // https://github.com/samclarke/SCEditor
+    // https://www.sceditor.com/
+    
+    Theme::add('https://cdn.jsdelivr.net/npm/sceditor@3/minified/themes/default.min.css');
+    Theme::add('https://cdn.jsdelivr.net/npm/sceditor@3/minified/sceditor.min.js');
+    Theme::add('https://cdn.jsdelivr.net/npm/sceditor@3/minified/formats/xhtml.min.js');
+    
+    $field = transFields(get_defined_vars());
+    $field->attributes['style'] = 'height:300px;width:100%;';
+    $field->attributes['id'] = $name;
 @endphp
 
 
@@ -21,8 +21,7 @@ $field->attributes['id'] = $name;
     @endslot
 @endcomponent
 
-
-
+{{-- dddx(Storage::disk('public_html')->url('assets/sceditor/emoticons')) --}}
 
 @push('scripts')
     <script>
@@ -30,7 +29,9 @@ $field->attributes['id'] = $name;
 
         sceditor.create(textarea, {
             format: 'xhtml',
-            style: 'https://cdn.jsdelivr.net/npm/sceditor@3/minified/themes/content/default.min.css'
+            style: 'https://cdn.jsdelivr.net/npm/sceditor@3/minified/themes/content/default.min.css',
+            // Le immagini si trovano scaricando lo zip da https://www.sceditor.com/
+            emoticonsRoot: '{{ Storage::disk('public_html')->url('assets/sceditor/') }}'
         });
     </script>
 @endpush
