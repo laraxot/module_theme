fa74387746a7c2152694b5034c26d33e91327ff4
Malebestia <malebestia@yahoo.it>
2022-10-21 12:13:08 +0200

    .

--- Resources/views/collective/fields/wysiwyg/sceditor/field.blade.php
+++ Resources/views/collective/fields/wysiwyg/sceditor/field.blade.php
@@ -20,8 +20,8 @@
         {{ Form::textarea($name, $value, $field->attributes) }}
     @endslot
 @endcomponent
-
-{{-- dddx(Storage::disk('public_html')->url('assets/sceditor/emoticons')) --}}
+{{-- {{ dddx(url('assets/sceditor')) }} --}}
+{{-- {{ dddx(Storage::disk('public_html')->url('assets/sceditor/emoticons')) }} --}}
 
 @push('scripts')
     <script>
