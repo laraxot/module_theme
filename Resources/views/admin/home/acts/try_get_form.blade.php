497f8b59e0f566e2c94b7fda03c82d0f0ce3577a
Davide Cavallini <davidecavallini1987@gmail.com>
2022-10-24 16:14:07 +0000

    .

--- Resources/views/admin/home/acts/try_get_form.blade.php
+++ Resources/views/admin/home/acts/try_get_form.blade.php
@@ -3,6 +3,7 @@
     <h3>Form</h3>
     <form action="/it/model/create">
         <livewire:form.get form_name="antonio" />
-        <input type="submit" value="Submit" class="btn btn-info mt-3">
+        <input type="submit" value="Save" class="btn btn-info mt-3">
+        <input type="submit" value="Reset" class="btn btn-danger mt-3">
     </form>
 @endsection
