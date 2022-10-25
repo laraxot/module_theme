6653ca092dfc6f354432584fd35c1fc53296acb9
marco xot <marco.sottana@gmail.com>
2022-10-25 13:10:04 +0200

    .

--- Resources/views/livewire/input/arr/elastic/filter_v3.blade.php
+++ Resources/views/livewire/input/arr/elastic/filter_v3.blade.php
@@ -6,7 +6,7 @@
 
     @foreach ($form_data[$name] ?? [] as $k => $v)
         @php
-            $input_name = '' . $name . '[' . $k . ']';
+            $input_name = $name . '[' . $k . ']';
             $wire_name = 'form_data.' . $name . '.' . $k;
         @endphp
 
@@ -62,8 +62,8 @@
             <div class="form-check">
                 <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
                 <a href="#" class="btn btn-outline-primary btn-sm rounded-pill" data-bs-toggle="modal"
-                    data-bs-target="#modal-02">TV Nazionali</a>
-                <div class="modal" id="modal-02" tabindex="-1">
+                    data-bs-target="#modal-00">TV Nazionali</a>
+                <div class="modal" id="modal-00" tabindex="-1">
                     <div class="modal-dialog modal-dialog-centered modal-lg">
                         <div class="modal-content text-center">
                             <div class="modal-body">
@@ -72,85 +72,161 @@
                                 <h4>Clicca sui singoli canali per disabilitarli o abilitarli
                                 </h4>
                                 <div class="container">
-                                    <div class="row">
+
+                                    @php
+                                        $i = 0;
+                                    @endphp
+
+                                    @foreach ($profile->channels()->where('type', 'Nazionali')->get() as $channel)
+                                        {{-- dddx($channel->name) --}}
+                                        @if ($i % 3 === 0)
+                                            <div class="row">
+                                        @endif
                                         <div class="col-4 py-8">
                                             <a href="#" data-bs-toggle="tooltip" data-bs-placement="top"
-                                                title="Rai1"><img
-                                                    src="{{ Theme::asset('pub_theme::img/canali/Rai1Logo.svg.png') }}"
+                                                title="{{ $channel->name }}"><img src="{{ $channel->logo }}"
                                                     height="50" width="65"></a>
                                         </div>
-                                        <div class="col-4 py-8">
-                                            <a href="#" data-bs-toggle="tooltip" data-bs-placement="top"
-                                                title="Rai2"><img
-                                                    src="{{ Theme::asset('pub_theme::img/canali/Rai2Logo.svg.png') }}"
-                                                    height="50" width="65"></a>
-                                        </div>
-                                        <div class="col-4 py-8">
-                                            <img src="{{ Theme::asset('pub_theme::img/canali/Rai3Logo.svg.png') }}"
-                                                height="50" width="65">
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-4 py-8">
-                                            <img src="{{ Theme::asset('pub_theme::img/canali/Rete4Logo.svg.png') }}"
-                                                height="50" width="50">
-                                        </div>
-                                        <div class="col-4 py-8">
-                                            <img src="{{ Theme::asset('pub_theme::img/canali/Canale5Logo.svg.png') }}"
-                                                height="50" width="50">
-                                        </div>
-                                        <div class="col-4 py-8">
-                                            <img src="{{ Theme::asset('pub_theme::img/canali/Italia1Logo.svg.png') }}"
-                                                height="50" width="50">
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-4 py-8">
-                                            <img src="{{ Theme::asset('pub_theme::img/canali/LA7Logo.png') }}"
-                                                height="50" width="60">
-                                        </div>
-                                        <div class="col-4 py-8">
-                                            <img src="{{ Theme::asset('pub_theme::img/canali/TV8Logo.svg.png') }}"
-                                                height="50" width="40">
-                                        </div>
-                                        <div class="col-4 py-8">
-                                            <img src="{{ Theme::asset('pub_theme::img/canali/NoveLogo.svg.png') }}"
-                                                height="50" width="50">
-                                        </div>
-                                    </div>
+                                        {{-- $i % 3 --}}
+                                        @if ($i > 0 && $i % 3 === 2)
                                 </div>
+                                @endif
+
+                                @php
+                                    $i++;
+                                @endphp
+                                @endforeach
+
                             </div>
-                            <!--/.modal-content -->
                         </div>
-                        <!--/.modal-body -->
                     </div>
-                    <!--/.modal-dialog -->
+                    <!--/.modal-content -->
                 </div>
-                <!--/.modal -->
+                <!--/.modal-body -->
             </div>
+            <!--/.modal-dialog -->
         </div>
-        <div class="form-select-wrapper mb-4 col-md-3 col-sm-6">
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked2" checked>
-                <a href="#" class="btn btn-outline-primary btn-sm  rounded-pill">TV
-                    Regionali</a>
-            </div>
-        </div>
-        <div class="form-select-wrapper mb-4 col-md-3 col-sm-6">
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked3" checked>
-                <a href="#" class="btn btn-outline-primary btn-sm  rounded-pill">Radio
-                    Nazionali</a>
-            </div>
-        </div>
-        <div class="form-select-wrapper mb-4 col-md-3 col-sm-6">
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked4" checked>
-                <a href="#" class="btn btn-outline-primary btn-sm  rounded-pill">Internazionali</a>
-            </div>
-        </div>
-    </div>
-    <div class="col-12 my-5">
-        <button class="btn btn-primary align-content-center col-lg-12 col-12">Cerca</button>
+        <!--/.modal -->
     </div>
 </div>
+<div class="form-select-wrapper mb-4 col-md-3 col-sm-6">
+    <div class="form-check">
+        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked2" checked>
+        <a href="#" class="btn btn-outline-primary btn-sm  rounded-pill">TV
+            Regionali</a>
+    </div>
+</div>
+<div class="form-select-wrapper mb-4 col-md-3 col-sm-6">
+    <div class="form-check">
+        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked3" checked>
+        <a href="#" class="btn btn-outline-primary btn-sm rounded-pill" data-bs-toggle="modal"
+                    data-bs-target="#modal-02">Radio Nazionali</a>
+            <div class="modal" id="modal-02" tabindex="-1">
+                <div class="modal-dialog modal-dialog-centered modal-lg">
+                    <div class="modal-content text-center">
+                        <div class="modal-body">
+                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                aria-label="Close"></button>
+                            <h4>Clicca sui singoli canali per disabilitarli o abilitarli
+                            </h4>
+                            <div class="container">
+
+                                @php
+                                    $i = 0;
+                                @endphp
+
+                                @foreach ($profile->channels()->where('type', 'NazionaliRadio')->get() as $channel)
+                                    {{-- dddx($channel->name) --}}
+                                    @if ($i % 3 === 0)
+                                        <div class="row">
+                                    @endif
+                                    <div class="col-4 py-8">
+                                        <a href="#" data-bs-toggle="tooltip" data-bs-placement="top"
+                                            title="{{ $channel->name }}"><img src="{{ $channel->logo }}"
+                                                height="50" width="65"></a>
+                                    </div>
+                                    {{-- $i % 3 --}}
+                                    @if ($i > 0 && $i % 3 === 2)
+                            </div>
+                            @endif
+
+                            @php
+                                $i++;
+                            @endphp
+                            @endforeach
+
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <!--/.modal-content -->
+            </div>
+            <!--/.modal-body -->
+        </div>
+    </div>
+</div>
+<div class="form-select-wrapper mb-4 col-md-3 col-sm-6">
+    <div class="form-check">
+        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked4" checked>
+        <a href="#" class="btn btn-outline-primary btn-sm rounded-pill" data-bs-toggle="modal"
+                    data-bs-target="#modal-03">Internazionali</a>
+            <div class="modal" id="modal-03" tabindex="-1">
+                <div class="modal-dialog modal-dialog-centered modal-lg">
+                    <div class="modal-content text-center">
+                        <div class="modal-body">
+                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                aria-label="Close"></button>
+                            <h4>Clicca sui singoli canali per disabilitarli o abilitarli
+                            </h4>
+                            <div class="container">
+
+                                @php
+                                    $i = 0;
+                                @endphp
+
+                                @foreach ($profile->channels()->where('type', 'Internazionali')->get() as $channel)
+                                    {{-- dddx($channel->name) --}}
+                                    @if ($i % 3 === 0)
+                                        <div class="row">
+                                    @endif
+                                    <div class="col-4 py-8">
+                                        <a href="#" data-bs-toggle="tooltip" data-bs-placement="top"
+                                            title="{{ $channel->name }}"><img src="{{ $channel->logo }}"
+                                                height="50" width="65"></a>
+                                    </div>
+                                    {{-- $i % 3 --}}
+                                    @if ($i > 0 && $i % 3 === 2)
+                            </div>
+                            @endif
+
+                            @php
+                                $i++;
+                            @endphp
+                            @endforeach
+
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <!--/.modal-content -->
+            </div>
+            <!--/.modal-body -->
+        </div>
+    </div>
+</div>
+</div>
+<div class="col-12 my-5">
+    <button class="btn btn-primary align-content-center col-lg-12 col-12">Cerca</button>
+</div>
+</div>
+
+{{-- dddx($profile->channels()->take(1)->get()) --}}
+
+@push('scripts')
+    <script>
+        function addTilde(element) {
+            const el = $('[name="' + element + '"]').not('#search_search')
+            $(el).val($(el).val() + '~')
+        }
+    </script>
+@endpush
