1b121a30de03f4169262321ecd14ae0719f91de1
marco xot <marco.sottana@gmail.com>
2022-10-31 18:05:27 +0100

    up

--- Resources/views/livewire/input/arr/elastic/filter_v3.blade.php
+++ Resources/views/livewire/input/arr/elastic/filter_v3.blade.php
@@ -24,18 +24,22 @@
             <input type="text" class="form-control" name="{{ $input_name }}[q]"
                 wire:model.lazy="{{ $wire_name }}.q" />
             @if (isset($form_data[$name][$k]['criteria']) && $form_data[$name][$k]['criteria'] != 'query_string_query')
-                <div class="input-group-text">
-                    <input class="form-check-input mt-0" type="checkbox" name="{{ $input_name }}[fuzzy]"
+                <div class="input-group-text my-2">
+                    <input class="form-check-input" type="checkbox" name="{{ $input_name }}[fuzzy]"
                         wire:model.lazy="{{ $wire_name }}.fuzzy"
                         aria-label="Checkbox for following text input" />&nbsp;Parola Simile
                 </div>
             @endif
         </div>
+        <a href="#" wire:click="subArr({{ $k }})" class="btn btn-circle btn-primary btn-sm"><i
+                class="uil uil-minus"></i></a>
+        <a href="#" onclick="addTilde()" class="btn btn-circle btn-primary btn-sm"><i
+                class="uil uil-tilde"></i></a>
     @endforeach
 
     <div class="row">
         <div class="form-select-wrapper mb-4 col-md-4">
-                <x-input.group type="select" name="order_by" :options="['desc' => 'Discendente', 'asc' => 'Ascendente']" label_class="h3" id="order_by" />
+            <x-input.group type="select" name="order_by" :options="['desc' => 'Discendente', 'asc' => 'Ascendente']" label_class="h3" id="order_by" />
         </div>
         <div class="form-select-wrapper mb-4 col-md-4">
             <x-input.group type="date.datetime.range" name="calendar" id="calendar" label_class="h3" />
@@ -44,22 +48,7 @@
             <x-input.group type="select" name="type" :options="$_theme->getMediaTypeList()" label_class="h3" id="media_type" />
         </div>
     </div>
-    {{--
-    <h3>Canali</h3>
-    <p>
-        <i class="uil uil-info-circle"></i>
-        Clicca sui bottoni per selezionare i singoli canali
-    </p>
-        <livewire:channel.choose />
-    --}}
-    {{--
-    <div class="col-12 my-5">
-        <button class="btn btn-primary align-content-center col-lg-12 col-12">Cerca</button>
-    </div>
-    --}}
 </div>
-
-{{-- dddx($profile->channels()->take(1)->get()) --}}
 
 @push('scripts')
     <script>
