6fc10d05f801ad340f9f86d921c58154a806e216
marco xot <marco.sottana@gmail.com>
2022-11-02 15:34:25 +0100

    up

--- Resources/views/components/card/v2.blade.php
+++ Resources/views/components/card/v2.blade.php
@@ -1,45 +1,48 @@
 <div class="card-wrapper shadow-sm rounded cmp-list-card-img">
-	<div class="card card-img no-after rounded">
-		<div class="img-responsive-wrapper cmp-list-card-img__wrapper">
-			<div class="img-responsive img-responsive-panoramic h-100">
-				@if(isset($img_src))
-				<figure class="img-wrapper">
-					<img class="rounded-top img-fluid" src="{{ $img_src }}" title="titolo immagine" alt="descrizione immagine">
-				</figure>
-				@endif
-                @if(isset($calendar))
+    <div class="card card-img no-after rounded">
+        <div class="img-responsive-wrapper cmp-list-card-img__wrapper">
+            <div class="img-responsive img-responsive-panoramic h-100">
+                @if (isset($img_src))
+                    <figure class="img-wrapper">
+                        <img class="rounded-top img-fluid" src="{{ $img_src }}" title="titolo immagine"
+                            alt="descrizione immagine">
+                    </figure>
+                @endif
+                @if (isset($calendar))
                     @php
-                        $tmp = explode(" ",$calendar);
-                        $type_date = ['date', 'day', 'hours']
+                        $tmp = explode(' ', $calendar);
+                        $type_date = ['date', 'day', 'hours'];
                     @endphp
                     <div class="card-calendar d-flex flex-column justify-content-center">
-                        @foreach($tmp as $key => $value)
+                        @foreach ($tmp as $key => $value)
                             <span class="card-{{ $type_date[$key] }}">{{ $tmp[$key] }}</span>
                         @endforeach
                     </div>
                 @endif
-			</div>
-		</div>
-		<div class="card-body">
-			@if(isset($category))
-			<div class="category-top cmp-list-card-img__body">
-				<a class="text-decoration-none fw-bold cmp-list-card-img__body-heading-title" href="{{ $url_category }}">{{ $category }}</a>
-                @if(isset($date))
-                    <span class="data">{{ $date }}</span>
-                @endif
-			</div>
-			@endif
-			<h3 class="cmp-list-card-img__body-title">
-				<a href="{{ $url ?? '#'}}" class="text-decoration-none">{{ $title }}</a>
-			</h3>
-			<p class="cmp-list-card-img__body-description">{{ $txt ?? '' }}</p>
-			<a class="read-more t-primary text-uppercase cmp-list-card-img__body-link" href="#" aria-label="Leggi di più sulla pagina di {{ $title }}">
-				<span class="text">Leggi di più</span>
-				<span class="visually-hidden">su {{ $title }}</span>
-				<svg class="icon icon-primary icon-xs ml-10">
-					<use href="../assets/bootstrap-italia/dist/svg/sprites.svg#it-arrow-right"></use>
-				</svg>
-			</a>
-		</div>
-	</div>
+            </div>
+        </div>
+        <div class="card-body">
+            @if (isset($category))
+                <div class="category-top cmp-list-card-img__body">
+                    <a class="text-decoration-none fw-bold cmp-list-card-img__body-heading-title"
+                        href="{{ $url_category }}">{{ $category }}</a>
+                    @if (isset($date))
+                        <span class="data">{{ $date }}</span>
+                    @endif
+                </div>
+            @endif
+            <h3 class="cmp-list-card-img__body-title">
+                <a href="{{ $url ?? '#' }}" class="text-decoration-none">{{ $title }}</a>
+            </h3>
+            <p class="cmp-list-card-img__body-description">{{ $txt ?? '' }}</p>
+            <a class="read-more t-primary text-uppercase cmp-list-card-img__body-link" href="#"
+                aria-label="Leggi di più sulla pagina di {{ $title }}">
+                <span class="text">Leggi di più</span>
+                <span class="visually-hidden">su {{ $title }}</span>
+                <svg class="icon icon-primary icon-xs ml-10">
+                    <use href="../assets/bootstrap-italia/dist/svg/sprites.svg#it-arrow-right"></use>
+                </svg>
+            </a>
+        </div>
+    </div>
 </div>
--- Resources/views/livewire/input/arr/elastic/filter_v3.blade.php
+++ Resources/views/livewire/input/arr/elastic/filter_v3.blade.php
@@ -10,19 +10,39 @@
             $wire_name = 'form_data.' . $name . '.' . $k;
         @endphp
 
-        <div class="form-select-wrapper mb-4">
-            <select class="form-select" name="{{ $input_name }}[criteria]"
-                wire:model.lazy="{{ $wire_name }}.criteria">
-                <option value="query_string_query" selected>Query diretta:</option>
-                <option value="must">Deve contenere:</option>
-                <option value="mustNot">Non deve contenere:</option>
-                <option value="should">Potrebbe conterere (or):</option>
-                <option value="regexp">Inizia con:</option>
-            </select>
+<div class="row">
+    
+        <div class="form-select-wrapper mb-4 col-10">
+            <div class="input-group">
+                <select class="form-select" name="{{ $input_name }}[criteria]"
+                    wire:model.lazy="{{ $wire_name }}.criteria">
+                    <option value="query_string_query" selected>Query diretta:</option>
+                    <option value="must">Deve contenere:</option>
+                    <option value="mustNot">Non deve contenere:</option>
+                    <option value="should">Potrebbe conterere (or):</option>
+                    <option value="regexp">Inizia con:</option>
+                </select>
+            </div>
+
         </div>
+
+            
+            <div class="input-group-append col-2 my-1">
+                <a href="#" wire:click="subArr({{ $k }})" class="btn btn-circle btn-primary btn-sm">
+                        <i class="uil uil-minus"></i>
+                    </a>
+                    <a href="#" onclick="addTilde('{{ $input_name }}[q]')" class="btn btn-circle btn-primary btn-sm">
+                        <i>~</i>
+                    </a>
+            </div>
+
+    
+</div>
+    
         <div class="form-floating mb-4">
-            <input type="text" class="form-control" name="{{ $input_name }}[q]"
+            <input type="text" id="textInputInserisci" placeholder="Text Input" class="form-control" name="{{ $input_name }}[q]"
                 wire:model.lazy="{{ $wire_name }}.q" />
+                <label for="textInputInserisci">Ricerca parola</label>
             @if (isset($form_data[$name][$k]['criteria']) && $form_data[$name][$k]['criteria'] != 'query_string_query')
                 <div class="input-group-text my-2">
                     <input class="form-check-input" type="checkbox" name="{{ $input_name }}[fuzzy]"
@@ -31,21 +51,21 @@
                 </div>
             @endif
         </div>
-        <a href="#" wire:click="subArr({{ $k }})" class="btn btn-circle btn-primary btn-sm"><i
-                class="uil uil-minus"></i></a>
-        <a href="#" onclick="addTilde()" class="btn btn-circle btn-primary btn-sm"><i
-                class="uil uil-tilde"></i></a>
+    
+        
+        
     @endforeach
+
 
     <div class="row">
         <div class="form-select-wrapper mb-4 col-md-4">
-            <x-input.group type="select" name="order_by" :options="['desc' => 'Discendente', 'asc' => 'Ascendente']" label_class="h3" id="order_by" />
+            <x-input.group type="select" class="form-select" name="order_by" :options="['desc' => 'Discendente', 'asc' => 'Ascendente']" label_class="h3" id="order_by" />
         </div>
         <div class="form-select-wrapper mb-4 col-md-4">
-            <x-input.group type="date.datetime.range" name="calendar" id="calendar" label_class="h3" />
+            <x-input.group type="date.datetime.range" class="form-select" name="calendar" id="calendar" label_class="h3" />
         </div>
         <div class="form-select-wrapper mb-4 col-md-4">
-            <x-input.group type="select" name="type" :options="$_theme->getMediaTypeList()" label_class="h3" id="media_type" />
+            <x-input.group type="select" class="form-select" name="type" :options="$_theme->getMediaTypeList()" label_class="h3" id="media_type" />
         </div>
     </div>
 </div>
