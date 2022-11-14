2840469f18402e950321f5ddb984d71e92847f88
marco xot <marco.sottana@gmail.com>
2022-11-14 16:33:25 +0100

    .

--- Resources/views/livewire/modal/body_view.blade.php
+++ Resources/views/livewire/modal/body_view.blade.php
@@ -24,7 +24,6 @@
                         </div>
                     @endif
 
-
                     @include($body_view)
 
                 </div>
--- View/Components/Modal.php
+++ View/Components/Modal.php
@@ -8,47 +8,30 @@ use Illuminate\Contracts\Support\Renderable;
 use Illuminate\View\Component;
 
 class Modal extends Component {
-    public string $id;
-    public string $title;
-    public ?string $size;
-    public bool $centered;
-    public int $index;
     public ?string $type;
     public array $attrs = [];
 
     public function __construct(
-        string $title,
-        ?string $size,
-        string $id,
-        bool $centered,
-        int $index,
-        ?string $type
+        ?string $type = 'v1'
     ) {
-        $this->id = $id;
-        $this->title = $title;
-        $this->centered = $centered;
-        $this->size = $size;
-        $this->index = $index;
         $this->type = $type;
-        $this->attrs['class'] = 'modal fade';
-        // $this->attrs['id'] =$id ;
-        $this->attrs['style'] = 'z-index: '.$this->zindex();
+        // $this->attrs['class'] = 'modal fade';
+        // $this->attrs['style'] = 'z-index: '.$this->zindex();
     }
 
-    public function modalsize(): string {
-        return null !== $this->size ? 'modal-'.$this->size : '';
-    }
-
+    // public function modalsize(): string {
+    //    return null !== $this->size ? 'modal-'.$this->size : '';
+    // }
+    /*
     public function zindex(): int {
         return $this->index * 1050;
     }
+    */
 
     public function render(): Renderable {
-        $view = 'theme::components.modal';
-        if (! empty($this->type)) {
-            $view .= '.'.$this->type;
-        }
+        $view = 'theme::components.modal.'.$this->type;
+        $view_params = [];
 
-        return view()->make($view);
+        return view($view, $view_params);
     }
 }
