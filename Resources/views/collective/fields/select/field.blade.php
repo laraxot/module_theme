96f254f4a19381adc899f931bcca06a7214b0bb7
marco xot <marco.sottana@gmail.com>
2022-11-24 17:06:06 +0100

    up

--- Resources/views/collective/fields/select/field.blade.php
+++ Resources/views/collective/fields/select/field.blade.php
@@ -6,6 +6,11 @@
     }
     // extract($attributes);
     // $field = transFields(get_defined_vars());
+    $val = Form::getValueAttribute($field->name);
+    if(is_object($val)){
+        //dddx([$field->name,$val,]);
+        $val=$val->getKey();
+    }
 @endphp
 
 @component($blade_component, get_defined_vars())
@@ -13,6 +18,9 @@
         {{ Form::label($name, $field->label, ['class' => 'control-label']) }}
     @endslot
     @slot('input')
+        {{--
         {{ Form::select($name, $options, $value, $field->attributes) }}
+        --}}
+        {{ Form::select($name, $options, $val, $field->attributes) }}
     @endslot
 @endcomponent
