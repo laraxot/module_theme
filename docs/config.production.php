98e3488effec67ce07bcb3ed1c980b387870f832
marco xot <marco.sottana@gmail.com>
2022-12-07 09:30:37 +0100

    .

--- docs/config.production.php
+++ docs/config.production.php
@@ -1,7 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
 return [
-    'baseUrl' => 'https://jigsaw-docs-template.tighten.co',
+    'baseUrl' => 'https://laraxot.github.io/module_theme',
     'production' => true,
 
     // DocSearch credentials
