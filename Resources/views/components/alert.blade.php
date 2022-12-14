30afee415e75e88cfa1a25d9318ec80f3cd8c529
Malebestia <malebestia@yahoo.it>
2022-12-14 18:36:14 +0100

    .

--- Resources/views/components/alert.blade.php
+++ Resources/views/components/alert.blade.php
@@ -3,7 +3,7 @@ https://getbootstrap.com/docs/4.5/components/alerts/
 --}}
 <div class="alert alert-{{$type}} {{($dismissable) ? 'alert-dismissible fade show' : ''}}">
     @if($dismissable)
-    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     @endif
 
     <h4 class="alert-heading"><i class="icon fas fa-{{$icon}}"></i> {{$title}}</h4>
