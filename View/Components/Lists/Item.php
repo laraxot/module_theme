8c9bd5c0863a798b968c41aff29b7e0b81602340
marco xot <marco.sottana@gmail.com>
2022-12-15 12:43:01 +0100

    up

--- View/Components/Lists/Item.php
+++ View/Components/Lists/Item.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Modules\Theme\View\Components\Lists;
 
 use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\View\Component;
 
 /**
