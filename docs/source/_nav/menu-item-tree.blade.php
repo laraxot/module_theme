7599129a7899933554305eeb13e4b0fbcc7764f4
marco76tv <marco.sottana@gmail.com>
2022-12-16 09:47:36 +0100

    .

--- docs/source/_nav/menu-item-tree.blade.php
+++ docs/source/_nav/menu-item-tree.blade.php
@@ -1,5 +1,5 @@
 <li class="pl-4">
-    <a href="{{ $item->getPath() }}"
+    <a href="{{ url($item->getPath()) }}"
         class="{{ 'lvl' . $level }} {{ $page->isItemActive($item) ? 'active font-semibold text-blue-500' : '' }} nav-menu__item hover:text-blue-500"
         >
         {{ $item->title }}
