14529562c87050b7409e26cc438edad0f670fc2d
Malebestia <malebestia@yahoo.it>
2022-12-21 11:10:45 +0100

    up

--- Resources/views/components/input/checkbox/field.blade.php
+++ Resources/views/components/input/checkbox/field.blade.php
@@ -1,3 +1,13 @@
 {{-- dddx(['attrs'=>$attrs,'attributes'=>$attributes]) --}}
-<input type="checkbox" {{ $attributes->merge($attrs) }} />
+{{-- <input type="checkbox" {{ $attributes->merge($attrs) }} /> --}}
 {{-- <input class="form-check-input" id="loginRemember" type="checkbox" /> --}}
+
+
+{{-- {{ dddx($options) }} --}}
+
+<div class="form-check">
+    @foreach ($options as $key => $option)
+        <input type="checkbox" {{ $attributes->merge($attrs) }} value="{{ $key }}"">
+        <label for="{{ $attrs['name'] }}"> {{ $option }}</label><br>
+    @endforeach
+</div>
--- Resources/views/components/input/radio/field.blade.php
+++ Resources/views/components/input/radio/field.blade.php
@@ -1,4 +1,16 @@
-<input type="radio" {{ $attributes->merge($attrs) }} />
+{{-- <input type="radio" {{ $attributes->merge($attrs) }} /> --}}
 {{--
 https://github.com/italia/design-comuni-pagine-statiche/blob/main/src/components/radio/radio.hbs
 --}}
+{{-- {{ dddx($attributes->merge($attrs)) }} --}}
+{{-- {{ dddx(['attrs' => $attrs, 'attributes' => $attributes]) }} --}}
+{{-- {{ dddx(get_defined_vars()) }} --}}
+{{-- {{ dddx($options) }} --}}
+{{-- {{ dddx($attrs['name']) }} --}}
+<div class="form-check">
+    @foreach ($options as $key => $option)
+        <input type="radio" {{ $attributes->merge($attrs) }} value="{{ $key }}">
+        <label for="{{ $attrs['name'] }}">{{ $option }}</label>
+        <br>
+    @endforeach
+</div>
