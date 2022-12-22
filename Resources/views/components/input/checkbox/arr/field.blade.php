7a503433eec9c4caa296e38afcea71e0d3de75d2
Malebestia <malebestia@yahoo.it>
2022-12-22 19:55:58 +0100

    up

--- Resources/views/components/input/checkbox/arr/field.blade.php
+++ Resources/views/components/input/checkbox/arr/field.blade.php
@@ -1,7 +1,13 @@
 <div class="form-check">
     {{-- dddx([get_defined_vars(),$attributes->merge($attrs)]) --}}
     @foreach ($options as $key => $option)
+        @php
+            // dddx([$key, $option]);
+            $attrs['wire:model.lazy'] = 'form_data.' . $name . '.' . $key;
+        @endphp
         <input type="checkbox" {{ $attributes->merge($attrs) }} value="{{ $key }}">
         <label for="{{ $attrs['name'] }}"> {{ $option }}</label><br>
     @endforeach
+
+
 </div>
