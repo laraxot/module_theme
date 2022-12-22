75724ebf282a98b5f5dfc2814419477b8dad2174
marco76tv <marco76tv@users.noreply.github.com>
2022-12-22 17:16:39 +0000

    Lint

--- Services/ThemeViewService.php
+++ Services/ThemeViewService.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Modules\Theme\Services;
 
-
 use Modules\Cms\Services\PanelService;
 use Modules\Theme\Menu\Builder;
 
--- View/Components/Category/Lists.php
+++ View/Components/Category/Lists.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace Modules\Theme\View\Components\Category;
 
-use Illuminate\View\Component;
-use Illuminate\Support\Collection;
 use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Support\Collection;
+use Illuminate\View\Component;
 
 /**
  * Class Lists.
